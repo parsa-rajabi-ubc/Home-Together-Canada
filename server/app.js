ee131323354a3f9b79ae0f4330d2b7226ca626f4
Rachelle Gelden <43148498+rachellegelden@users.noreply.github.com>
2020-10-24 23:36:26 -0700

    HTC-64: setup to deploy to heroku (#116)

--- server/app.js
+++ server/app.js
@@ -16,9 +16,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const publicPath = path.join(__dirname, 'public');
+app.use(express.static(publicPath));
 
 app.use('/', routes);
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
