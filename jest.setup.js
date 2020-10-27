9884cee8525f708d51b04b887f8714ba13bac86f
Rachelle Gelden <rachelle@gelden.com>
2020-10-27 00:28:18 -0700

    HTC-64: added jest dependency

---
+++ jest.setup.js
@@ -0,0 +1,5 @@
+import '@testing-library/jest-dom/extend-expect';
+import "jest-dom/extend-expect";
+import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom'
+import {toBeInTheDocument, toHaveClass} from '@testing-library/jest-dom'
