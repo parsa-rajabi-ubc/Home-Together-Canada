e17da36e09ea54fa8bc196e6bad73b45c8bb3281
Rachelle Gelden <43148498+rachellegelden@users.noreply.github.com>
2020-10-27 21:56:15 -0700

    HTC-135: removed travis.yml config, removed import statement causing the issue. (#141)

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,4 +1,3 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
 import React from 'react';
 // import {toBeInTheDocument, toHaveClass} from '@testing-library/jest-dom'
