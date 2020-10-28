147464b2fb409b046f187aac5fce58cee60f73c3
Rachelle Gelden <43148498+rachellegelden@users.noreply.github.com>
2020-10-27 18:42:25 -0700

    HTC-64: added jest dependency (#128)

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import React from 'react';
+// import {toBeInTheDocument, toHaveClass} from '@testing-library/jest-dom'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe("test suite", () => {
+  it("test 1", () => {
+      expect(true).toBe(true);
+  })
+})
