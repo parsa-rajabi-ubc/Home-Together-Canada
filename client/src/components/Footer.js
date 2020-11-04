5a67d988db3016ecda858d0702a69c414ddaa099
Parsa <ParsaRajabiPR@gmail.com>
2020-11-03 22:49:23 -0800

    HTC-131: added footer to app and created Footer compoenent

---
+++ client/src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../tailwind.output.css';
+
+
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                <a href="#">About Us </a>
+                <a href="#">FAQ </a>
+                <a href="#">Contact Us </a>
+                <a href="#">Comment & Concerns Form </a>
+            </footer>
+            <div>
+                <p> Home Together Canada 2020 - All Rights Received</p>
+            </div>
+        </>
+    )
+}
+
+export default Footer;
