ac0027887e5a96019ea082ffe45796b32b61ed7a
Parsa <ParsaRajabiPR@gmail.com>
2020-11-03 21:03:37 -0800

    HTC-131: Added Header component to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react'
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+        <Header/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
