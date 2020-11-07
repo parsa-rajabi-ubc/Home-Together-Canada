00de01f83e4fdc47174c440b77222fb153cefad4
Parsa <ParsaRajabiPR@gmail.com>
2020-11-07 13:34:34 -0800

    HTC-131: removed un-used code in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react'
 import Header from './components/Header';
@@ -6,27 +5,13 @@ import Home from './components/Home';
 import Footer from './components/Footer';
 
 function App() {
-  return (
-    <div className="App">
-        <Header/>
-      {/*<header className="App-header">*/}
-      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
-      {/*  <p>*/}
-      {/*    Edit <code>src/App.js</code> and save to reload!*/}
-      {/*  </p>*/}
-      {/*  <a*/}
-      {/*    className="App-link"*/}
-      {/*    href="https://reactjs.org"*/}
-      {/*    target="_blank"*/}
-      {/*    rel="noopener noreferrer"*/}
-      {/*  >*/}
-      {/*    Learn React*/}
-      {/*  </a>*/}
-      {/*</header>*/}
-      <Home/>
-        <Footer/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Home/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
