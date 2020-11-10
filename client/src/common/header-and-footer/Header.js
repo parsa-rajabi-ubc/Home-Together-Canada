19459ff3557a6589509b57289d06c34585a4995c
Parsa Rajabi <ParsaRajabiPR@gmail.com>
2020-11-09 23:41:22 -0800

    HTC-131: Created Header, Footer and Home Components  (#190)
    * HTC-131: added header, footer, home componenets + image
    
    * HTC-131: added header, footer, home to App.js
    
    * HTC-131: removed unused code

---
+++ client/src/common/header-and-footer/Header.js
@@ -0,0 +1,60 @@
+/**
+ * @Author:     Parsa Rajabi
+ * @Created:    2020.11.09
+ *
+ * @Description: header for the website which contains links to Connect with Members, Services, Classifieds, About Us and Account Page
+ *
+ */
+
+import React from 'react';
+import '../../tailwind.output.css';
+
+const Header = () => {
+    return (
+        <div>
+            <div className="realtive">
+                <div className="">
+                    <div className="flex justify-between items-center">
+                        <div>
+                            <a href="#" className="flex">
+                                Home Together Canada
+                            </a>
+                        </div>
+                        <nav className="md:flex space-x-10">
+                            <a href="#">
+                                Connect with Members
+                            </a>
+                            <a href="#">
+                                Services
+                            </a>
+                            <a href="#">
+                                Classifieds
+                            </a>
+                            <a href="#">
+                                About Us
+                            </a>
+                            <a href="#">
+                                FAQ
+                            </a>
+                        </nav>
+
+                        <div>
+                            {/*TODO: Add href to login page*/}
+                            <button href="#"
+                                    className="border">
+                                Login
+                            </button>
+                            {/*TODO: Add href to registration page*/}
+                            <button href="#"
+                                    className="border">
+                                Sign Up
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ client/src/home/Home.js
@@ -0,0 +1,48 @@
+/**
+ * @Author:     Parsa Rajabi
+ * @Created:    2020.11.09
+ *
+ * @Description: landing page for the website which contains a picture and some info about Home Together Canada
+ *
+ */
+
+import React from 'react';
+import '../tailwind.output.css';
+import picture from '../images/move_in.jpg';
+
+const Home = () => {
+    return (
+        <div>
+            <main>
+                <h2>Home Together
+                    <p>Canada</p>
+                </h2>
+                <p>
+                    A volunteer-run, non-profit Association to facilitate the growth and success of
+                    shared living in Canada.
+                    We do this by providing the two fundamental needs necessary to the success of both
+                    the individuals, wishing to share homes, and the service providers and businesses
+                    wishing to serve this burgeoning and diverse group.
+                </p>
+                <div>
+                    <div>
+                        <button href="#">
+                            Join Us
+                        </button>
+                    </div>
+                    <div>
+                        <button href="#">
+                            Learn More
+                        </button>
+                    </div>
+                </div>
+            </main>
+            <div>
+                <img src={picture} alt="group of adults moving into a house">
+                </img>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
