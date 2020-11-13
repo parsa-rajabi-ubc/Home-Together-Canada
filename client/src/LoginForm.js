fb84793a94ac975b289c9d1646e7187e59cddf26
QinAlex <43220166+QinAlex@users.noreply.github.com>
2020-11-12 20:30:11 -0800

    Update LoginForm.js

--- client/src/LoginForm.js
+++ client/src/LoginForm.js
@@ -1,25 +1,37 @@
-import React from "react";
+import React, {useState} from 'react'
 import Checkbox from './common/forms/Checkbox';
 import TextArea from './common/forms/TextArea';
+import LoginButton from './common/forms/LoginButton';
 
 export function LoginForm(){
 
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
+
         <div>
             {/*title of the business*/}
             Home-Together-Canada Login: <br/>
 
             {/*input user name*/}
-            <TextArea label="Username: " placeholder=""/> <br/>
+            <TextArea label="Username: "
+                      placeholder=""
+                      onChange= {(e) => {setUsername(e.target.value)}}
+            /> <br/>
 
             {/*input user password*/}
-            <TextArea label="Password: " placeholder=""/> <br/>
+            Password:
+            <input type="password" onChange = {(e) =>setPassword(e.target.value)} /> <br/>
 
             {/*remember me feature, using the checkbox function*/}
             <Checkbox label="Remember me."/> <br/>
 
             {/*login button*/}
-            <button>Login</button> <br/>
+            <LoginButton onClick={() => {
+                if(username.toString().trim() === "") alert ('Username required.');
+                if(password.toString().trim() === "") alert ('Password required.');
+            }}/> <br/>
 
             {/*link to PasswordRecovery page*/}
             <a href="">Forgot your password?</a> <br/>
@@ -27,5 +39,6 @@ export function LoginForm(){
             {/*link to SignIn page*/}
             <a href="">Do not have an account? Create one now.</a>
         </div>
+
     );
 }
