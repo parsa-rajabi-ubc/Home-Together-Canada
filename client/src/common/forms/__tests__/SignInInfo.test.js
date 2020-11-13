0ade2d1709d4c82afc4a1d119d0d4980f51ffa23
Jeff Hatton <lightbrite88@gmail.com>
2020-11-12 23:09:20 -0700

    HTC-21: Add when and then comments for readability

--- client/src/common/forms/__tests__/SignInInfo.test.js
+++ client/src/common/forms/__tests__/SignInInfo.test.js
@@ -4,7 +4,10 @@ import renderer from 'react-test-renderer';
 describe('SignInInfo', () => {
         describe('Snapshot test', () => {
                 it("should match stored snapshot", () => {
+                        //when
                         const component = renderer.create(<SignInInfo/>);
+
+                        //then
                         expect(component).toMatchSnapshot();
                 });
         })
