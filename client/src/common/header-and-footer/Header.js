7290c8229a4f06345631b3fe442456a4e9839a60
Parsa <ParsaRajabiPR@gmail.com>
2020-11-13 17:30:25 -0800

    HTC-216: added Links to Header.js

--- client/src/common/header-and-footer/Header.js
+++ client/src/common/header-and-footer/Header.js
@@ -8,6 +8,7 @@
 
 import React from 'react';
 import '../../tailwind.output.css';
+import {Link} from "react-router-dom";
 
 const Header = () => {
     return (
@@ -16,39 +17,63 @@ const Header = () => {
                 <div className="">
                     <div className="flex justify-between items-center">
                         <div>
-                            <a href="#" className="flex">
-                                Home Together Canada
-                            </a>
+                            <Link to={'/'}>
+                                <a className="flex">
+                                    Home Together Canada
+                                </a>
+                            </Link>
                         </div>
                         <nav className="md:flex space-x-10">
-                            <a href="#">
-                                Connect with Members
-                            </a>
-                            <a href="#">
-                                Services
-                            </a>
-                            <a href="#">
-                                Classifieds
-                            </a>
-                            <a href="#">
-                                About Us
-                            </a>
-                            <a href="#">
-                                FAQ
-                            </a>
+
+                            {/*TODO: updated this Link to appropriate page*/}
+                            <Link to={'/members'}>
+                                <a href="#">
+                                    Connect with Members
+                                </a>
+                            </Link>
+
+                            {/*TODO: updated this Link to appropriate page*/}
+                            <Link to={'/services'}>
+                                <a href="#">
+                                    Services
+                                </a>
+                            </Link>
+
+                            {/*TODO: updated this Link to appropriate page*/}
+                            <Link to={'/classifieds'}>
+                                <a href="#">
+                                    Classifieds
+                                </a>
+                            </Link>
+
+                            {/*TODO: updated this Link to appropriate page*/}
+                            <Link to={'/about'}>
+                                <a href="#">
+                                    About Us
+                                </a>
+                            </Link>
+
+                            {/*TODO: updated this Link to appropriate page*/}
+                            <Link to={'/faq'}>
+                                <a href="#">
+                                    FAQ
+                                </a>
+                            </Link>
                         </nav>
 
                         <div>
-                            {/*TODO: Add href to login page*/}
-                            <button href="#"
-                                    className="border">
-                                Login
-                            </button>
-                            {/*TODO: Add href to registration page*/}
-                            <button href="#"
-                                    className="border">
-                                Sign Up
-                            </button>
+                            <Link to={'/login'}>
+                                <button className="border">
+                                    Login
+                                </button>
+                            </Link>
+
+                            {/*TODO: Update this Link to registration landing page*/}
+                            <Link to={'/registration/business'}>
+                                <button className="border">
+                                    Sign Up
+                                </button>
+                            </Link>
                         </div>
                     </div>
                 </div>
