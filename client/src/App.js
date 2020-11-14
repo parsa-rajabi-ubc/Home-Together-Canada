fc10df2b77d3b75912393b73e8b9419fa8828db2
Parsa <ParsaRajabiPR@gmail.com>
2020-11-13 17:01:40 -0800

    HTC-183: added react-router-dom to project, added routing elemts to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,26 @@ import React from 'react'
 import Header from './common/header-and-footer/Header';
 import Home from './home/Home';
 import Footer from './common/header-and-footer/Footer';
+import Login from './LoginForm';
+import BusinessRegistration from './registration/BusinessRegistrationForm';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
-  return (
-    <div>
-      <Header/>
-      <Home/>
-      <Footer/>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <Header/>
+                {/* Matches the URL to the path and does not go through the rest of the routes*/}
+                <Switch>
+                    {/* Renders the correct component based on the URL*/}
+                    <Route path={"/"} exact component={Home}/>
+                    <Route path={"/login"} component={Login}/>
+                    <Route path={"/registration/business"} component={BusinessRegistration}/>
+                </Switch>
+                <Footer/>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
