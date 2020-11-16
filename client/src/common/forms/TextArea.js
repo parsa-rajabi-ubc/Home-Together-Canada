b713468b56918d0d47824fc9459a485801b9e469
QinAlex <43220166+QinAlex@users.noreply.github.com>
2020-11-15 21:56:32 -0800

    Update TextArea.js

--- client/src/common/forms/TextArea.js
+++ client/src/common/forms/TextArea.js
@@ -1,21 +1,20 @@
-import React, {useState} from 'react';
+import React from 'react';
 import PropTypes from "prop-types";
 
 //returns generic textbox field for a form with a label
 function TextArea(props){
-    const { label, onChange } = props;
+    const { onChange } = props;
 
     return(
         <label>
             {props.label}
-            <input type="text" placeholder={props.placeholder}  onChange= {onChange}/>
+            <input type="text" onChange= {onChange}/>
         </label>
     );
 }
 
 TextArea.propTypes = {
     label: PropTypes.string.isRequired,
-    placeholder: PropTypes.string.isRequired,
     onChange: PropTypes.func
 }
 
