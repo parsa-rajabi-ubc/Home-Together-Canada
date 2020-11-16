cce4de02e75f682034e85c19a4f9847f5ed97fdd
Rachelle Gelden <rachelle@gelden.com>
2020-11-15 18:15:33 -0800

    HTC-129: added reference to crypto documentation

--- server/services/PasswordService.js
+++ server/services/PasswordService.js
@@ -6,6 +6,7 @@ const getSalt = function() {
 
 const getHashedPassword = function(aPassword, aSalt) {
     if (!aPassword || !aSalt) return '';
+    // refer to https://nodejs.org/api/crypto.html#crypto_crypto_pbkdf2sync_password_salt_iterations_keylen_digest
     return crypto.pbkdf2Sync(aPassword, aSalt, 10000, 64, 'sha512').toString();
 };
 
