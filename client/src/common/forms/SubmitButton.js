4e13821f121f87e18cf7b651deca5668190a299e
QinAlex <43220166+QinAlex@users.noreply.github.com>
2020-11-15 21:57:19 -0800

    Update SubmitButton.js

--- client/src/common/forms/SubmitButton.js
+++ client/src/common/forms/SubmitButton.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import propTypes from "prop-types";
+import PropTypes from "prop-types";
 
 //returns generic Submit button field for a form with a label
 function SubmitButton(props){
+    const { onClick } = props;
+
     return(
         <label>
             {props.label}
-            <input type="submit" value="Submit" />
+            <input type="submit" value = {props.value} onClick={onClick}/>
         </label>
     );
 }
 
 SubmitButton.propTypes = {
-    label: propTypes.string.isRequired
+    label: PropTypes.string.isRequired,
+    onClick: PropTypes.func,
+    value: PropTypes.string.isRequired
 }
 
 export default SubmitButton
