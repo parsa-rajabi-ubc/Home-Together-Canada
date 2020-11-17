305ac5a76e4179f8e2b31d71c0173ca7fca2e96b
QinAlex <43220166+QinAlex@users.noreply.github.com>
2020-11-16 20:03:18 -0800

    Update registrationUtils.js

--- client/src/registration/registrationUtils.js
+++ client/src/registration/registrationUtils.js
@@ -7,13 +7,12 @@
  */
 
 const currentYear = new Date().getFullYear();
-const RegistrationUtils = [];
+const YEARS = [];
 
 for (let j = 0, i = currentYear-18; i >= currentYear - 98; i--,j++) {
-    RegistrationUtils.push({
+    YEARS.push({
         id : j,
         value : i}
     )
 }
-export default RegistrationUtils
-
+export default YEARS
