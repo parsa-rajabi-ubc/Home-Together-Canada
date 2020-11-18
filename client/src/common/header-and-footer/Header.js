105a53064121cdbd548fe5e82479ddc2088b99f9
Parsa <ParsaRajabiPR@gmail.com>
2020-11-17 18:54:39 -0800

    HTC-228: updated Header with new css

--- client/src/common/header-and-footer/Header.js
+++ client/src/common/header-and-footer/Header.js
@@ -13,59 +13,57 @@ import {Link} from "react-router-dom";
 const Header = () => {
     return (
         <div>
-            <div className="realtive">
-                <div className="">
-                    <div className="flex justify-between items-center">
-                        <div>
-                            <Link to={'/'} className="flex">
-                                Home Together Canada
-                            </Link>
-                        </div>
-                        <nav className="md:flex space-x-10">
+            {/*TODO: change bg color to igreen*/}
+            <nav className="top-0 flex w-full bg-green-400 tex-black">
+                <div className="container flex items-center justify-between w-full py-2 mx-auto mt-0 whitespace-no-wrap">
+                    <div className="flex flex-no-wrap items-center ml-4">
+                        <Link to={'/'}
+                              className="text-lg font-bold lg:text-xl">
+                            Home Together Canada
+                        </Link>
+                    </div>
 
-                            {/*TODO: updated this Link to appropriate page*/}
-                            <Link to={'/members'}>
+                    {/*TODO: move classNames to header.css */}
+                    {/* Middle of Nav */}
+                    <div
+                        className="flex-no-wrap hidden w-full p-4 mt-2 tex-black lg:flex lg:items-center lg:w-auto lg:block lg:mt-0 lg:bg-transparent lg:p-0">
+                        <div className="items-center justify-end flex-1 mr-16 list-reset lg:flex">
+                            <Link to={'/members'}
+                                  className="inline-block px-4 py-2 mr-3 transition duration-200 ease-in-out border-transparent rounded-md hover:bg-white hover:text-black hover:opacity-50">
                                 Connect with Members
                             </Link>
-
-                            {/*TODO: updated this Link to appropriate page*/}
-                            <Link to={'/services'}>
+                            <Link to={'/services'}
+                                  className="inline-block px-4 py-2 mr-3 transition duration-200 ease-in-out border-transparent rounded-md hover:bg-white hover:text-black hover:opacity-50">
                                 Services
                             </Link>
-
-                            {/*TODO: updated this Link to appropriate page*/}
-                            <Link to={'/classifieds'}>
+                            <Link to={'/classifieds'}
+                                  className="inline-block px-4 py-2 mr-3 transition duration-200 ease-in-out border-transparent rounded-md hover:bg-white hover:text-black hover:opacity-50">
                                 Classifieds
                             </Link>
-
-                            {/*TODO: updated this Link to appropriate page*/}
-                            <Link to={'/about'}>
+                            <Link to={'/about'}
+                                  className="inline-block px-4 py-2 mr-3 transition duration-200 ease-in-out border-transparent rounded-md hover:bg-white hover:text-black hover:opacity-50">
                                 About Us
                             </Link>
-
-                            {/*TODO: updated this Link to appropriate page*/}
-                            <Link to={'/faq'}>
+                            <Link to={'/faq'}
+                                  className="inline-block px-4 py-2 mr-3 no-underline transition duration-200 ease-in-out border-transparent rounded-md hover:bg-white hover:text-black hover:opacity-50">
                                 FAQ
                             </Link>
-                        </nav>
-
-                        <div>
-                            <Link to={'/login'}>
-                                <button className="border">
-                                    Login
-                                </button>
-                            </Link>
-
-                            {/*TODO: Update this Link to registration landing page*/}
-                            <Link to={'/registration/business'}>
-                                <button className="border">
-                                    Sign Up
-                                </button>
-                            </Link>
                         </div>
+
+                        {/* Login and Sign Up Buttons */}
+                        <Link to={'/login'}
+                              className="items-center justify-center w-full px-6 py-2 mr-4 transition duration-200 ease-in-out bg-white border-transparent rounded-md opacity-75 hover:bg-orange-400">Login
+                        </Link>
+                        <Link to={'/registration'}
+                              className="items-center justify-center w-full px-4 py-2 mr-4 transition duration-200 ease-in-out bg-white border-transparent rounded-md opacity-75 hover:bg-orange-400">Sign
+                            Up
+                        </Link>
+
                     </div>
                 </div>
-            </div>
+                {/*Bottom Border*/}
+                <hr className="py-0 my-0 border-b border-gray-200 opacity-25"/>
+            </nav>
         </div>
     )
 }
