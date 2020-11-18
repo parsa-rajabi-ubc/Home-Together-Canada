a89d4a53194c035c1f2c429b99bcd3594c83b0c2
lightbrite88 <lightbrite88@gmail.com>
2020-11-18 15:49:42 -0700

    HTC-87: creating function components and form component - incomplete

---
+++ client/src/common/forms/Cities.js
@@ -0,0 +1,31 @@
+/**
+ * @Author:     Jeff Hatton
+ * @Created:    2020.11.18
+ *
+ * @Description: Const list of cities.
+ *
+ */
+
+const cities = [
+    {
+        id: 1,
+        value: "City name 1"
+    },
+    {
+        id: 2,
+        value: "City name 2"
+    },
+    {
+        id: 3,
+        value: "City name 3"
+    },
+    {
+        id: 4,
+        value: "City name 4"
+    },
+    {
+        id: 5,
+        value: "City name 5"
+    }
+]
+export default cities;
---
+++ client/src/common/forms/ShareLimits.js
@@ -0,0 +1,31 @@
+/**
+ * @Author:     Jeff Hatton
+ * @Created:    2020.11.18
+ *
+ * @Description: Member profile static form Component. Returns a form.
+ *
+ */
+
+const shareLimits = [
+    {
+        id: 1,
+        value: "1"
+    },
+    {
+        id: 2,
+        value: "2"
+    },
+    {
+        id: 3,
+        value: "3"
+    },
+    {
+        id: 4,
+        value: "4"
+    },
+    {
+        id: 5,
+        value: "Any number"
+    }
+]
+export default shareLimits;
