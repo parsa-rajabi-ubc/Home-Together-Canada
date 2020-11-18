6f761710935f62a019103ef61e5326931598d8f2
Parsa Rajabi <ParsaRajabiPR@gmail.com>
2020-11-17 21:55:25 -0800

    HTC-228: updated Footer with new css

--- client/src/common/header-and-footer/Footer.js
+++ client/src/common/header-and-footer/Footer.js
@@ -14,37 +14,81 @@ import {Link} from "react-router-dom";
 const Footer = () => {
     return (
         <div>
-            <footer>
+            <footer className="bg-white">
+                <div className="container border-t-2 mx-auto  px-8">
+                    <div className="w-full flex flex-col md:flex-row py-6">
 
-                {/*TODO: updated this Link to appropriate page*/}
-                <Link to={'/about'}>
-                    About Us
-                </Link>
+                        <div className="flex-1 mb-6">
+                            {/*TODO: Update text color*/}
+                            <Link to={'/'} className="text-orange-500 font-bold text-2xl">
+                                Home Together Canada
+                            </Link>
+                            {/* TODO: Add HTC logo here after Twila sends it */}
+                        </div>
 
-                {/*TODO: updated this Link to appropriate page*/}
-                <Link to={'/faq'}>
-                    FAQ
-                </Link>
+                        {/*TODO: create a generic class with the following CSS and apply to all below*/}
 
-                {/*TODO: updated this Link to appropriate page*/}
-                <Link to={'/contact'}>
-                    Contact Us
-                </Link>
+                        <div className="flex-1">
+                            <p className="uppercase text-gray-500 md:mb-6">Links</p>
+                            <ul className="list-reset mb-6">
+                                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                                    <Link to={'/faq'}
+                                          className="text-gray-800 hover:text-orange-500">FAQ</Link>
+                                </li>
+                                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                                    <Link to={'/report'}
+                                          className="text-gray-800 hover:text-orange-500">Comment
+                                        & Concern Form</Link>
+                                </li>
+                            </ul>
+                        </div>
 
-                {/*TODO: updated this Link to appropriate page*/}
-                <Link to={'/report'}>
-                    Comment & Concerns Form
-                </Link>
+                        <div className="flex-1">
+                            <p className="uppercase text-gray-500 md:mb-6">Legal</p>
+                            <ul className="list-reset mb-6">
+                                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                                    <Link to={'/tos'}
+                                          className="text-gray-800 hover:text-orange-500">Terms</Link>
+                                </li>
+                                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                                    <Link to={'/tos'}
+                                          className="text-gray-800 hover:text-orange-500">Privacy</Link>
+                                </li>
+                            </ul>
+                        </div>
 
+                        <div className="flex-1">
+                            <p className="uppercase text-gray-500 md:mb-6">Social</p>
+                            <ul className="list-reset mb-6">
+                                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                                    <a href="https://www.facebook.com/WeAreHomeTogetherCanada/"
+                                       className="text-gray-800 hover:text-orange-500">Facebook</a>
+                                </li>
+                                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                                    <a href="https://www.happipad.com/housing-companion-program/"
+                                       className="text-gray-800 hover:text-orange-500">Happipad</a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div className="flex-1">
+                            <p className="uppercase text-gray-500 md:mb-6">Company</p>
+                            <ul className="list-reset mb-6">
+                                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                                    <Link to={'/about'}
+                                          className="text-gray-800 hover:text-orange-500">About Us</Link>
+                                </li>
+                                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                                    <a href="mailto:hometogether.ca@gmail.com"
+                                       className="text-gray-800 hover:text-orange-500">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+
+
+                    </div>
+                </div>
             </footer>
-            <div>
-                <p>
-                    <Link to={'/'}>
-                        Home Together Canada
-                    </Link>
-                    - All Rights Received
-                </p>
-            </div>
         </div>
     )
 }
