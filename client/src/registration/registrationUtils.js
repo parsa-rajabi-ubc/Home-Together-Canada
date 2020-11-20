971c7b1c90af113b6c2053d52b59d5beb8d2c903
QinAlex <qinzhenyu1123@gmail.com>
2020-11-19 21:33:29 -0800

    HTC-86: Small changed is fixed.

--- client/src/registration/registrationUtils.js
+++ client/src/registration/registrationUtils.js
@@ -9,7 +9,7 @@
 const currentYear = new Date().getFullYear();
 const YEARS = [];
 
-for (let j = 0, i = currentYear-18; i >= currentYear - 98; i--,j++) {
+for (let j = 0, i = currentYear-16; i >= currentYear - 96; i--,j++) {
     YEARS.push({
         id : j,
         value : i}
