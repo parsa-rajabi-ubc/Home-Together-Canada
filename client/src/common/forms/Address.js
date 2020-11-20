2917093599bdb6155fd0bbce167e95092121adef
QinAlex <qinzhenyu1123@gmail.com>
2020-11-19 19:09:48 -0800

    HTC-86: Change on Address and add vaildation on some parts of the Member static form.

--- client/src/common/forms/Address.js
+++ client/src/common/forms/Address.js
@@ -1,6 +1,3 @@
-import provinces from "./Provinces";
-import React from "react";
-import Dropdown from "./Dropdown";
 /**
  * @Author:     Jeff Hatton
  * @Created:    2020.11.05
@@ -8,29 +5,40 @@ import Dropdown from "./Dropdown";
  * @Description: Address input Form Component
  *
  */
-function Address(){
+
+import provinces from "./Provinces";
+import React from "react";
+import Dropdown from "./Dropdown";
+import PropTypes from "prop-types";
+
+
+function Address(props){
+    const { label, onChange } = props;
     return(
         <div>
             <label>
                 Address Line 1:
-                <input type="text" placeholder="Street Address" />
+                <input type="text" name="street" placeholder="Street Address" onChange= {onChange}/>
             </label>
             <label>
-                Address Line 2:
-                <input type="text" placeholder="Apt, suite, floor # etc." />
+                Optional:
+                <input type="text" name="aptNum" placeholder="Apt, suite, floor # etc." onChange= {onChange}/>
             </label>
             <label>
-                City:
-                <input type="text" placeholder="City" />
+
+                <input type="text" name="city" placeholder="City" onChange={onChange}/>
             </label>
-            <Dropdown title={"Province"} items={provinces}/>
+            <Dropdown name="province" title={"Province"} items={provinces} onChange={onChange}/>
             <label>
-                Postal Code:
-                <input type="text" placeholder="Postal Code" />
+
+                <input type="text" name="postalCode" placeholder="Postal Code" onChange= {onChange}/>
             </label>
         </div>
     );
 }
-
+Address.propTypes = {
+    label: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired
+}
 
 export default Address;
--- client/src/registration/MemberRegistrationForm.js
+++ client/src/registration/MemberRegistrationForm.js
@@ -19,6 +19,49 @@ import YEARS from "./registrationUtils";
 //Returns a Form with fields
 function MemberRegistrationForm() {
     const [isSameAddress, setIsSameAddress] = useState(false);
+    const [phoneNumber, setPhoneNumber] = useState({
+        first: "",
+        middle: "",
+        last: ""
+    });
+    const [address, setAddress] = useState({
+        street: "",
+        aptNum: "",
+        city: "",
+        province: "",
+        postalCode: ""
+    });
+    const [mailingAddress, setMailingAddress] = useState({
+        street: "",
+        aptNum: "",
+        city: "",
+        province: "",
+        postalCode: ""
+    });
+
+    function handlePhoneChange(e){
+        const value = e.target.value;
+        setPhoneNumber({
+            ...phoneNumber,
+            [e.target.name]: value
+        });
+    }
+
+    function handleAddressChange(e){
+        const value = e.target.value;
+        setAddress({
+            ...address,
+            [e.target.name]: value
+        });
+    }
+
+    function handleMailingAddress(e){
+        const value = e.target.value;
+        setMailingAddress({
+            ...mailingAddress,
+            [e.target.name]: value
+        });
+    }
 
     return (
         <div>
@@ -32,12 +75,12 @@ function MemberRegistrationForm() {
                 {/*change this later*/}
                 <Dropdown title={"Year of Birth"} items={YEARS}/>
 
-                <PhoneNumInput label="TelephoneNumber: "/>
+                <PhoneNumInput label="TelephoneNumber: " onChange={handlePhoneChange}/>
                 <TextArea label="Email Address: " placeholder=""/>
-                <Address/>
+                <Address onChange={handleAddressChange}/>
 
                 <Checkbox label="Different Mailing Address? " onChange= {() => {setIsSameAddress(!isSameAddress)}}/>
-                {isSameAddress && <Address label="Mailing Address: "/>}
+                {isSameAddress && <Address label="Mailing Address: " onChange={handleMailingAddress}/>}
 
                 <hr/>
                 <h2>Account Details</h2>
