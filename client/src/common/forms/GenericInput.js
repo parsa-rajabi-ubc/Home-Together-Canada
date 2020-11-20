83022f6ffb3d1a07882677a45e46294f7ca80782
Parsa <ParsaRajabiPR@gmail.com>
2020-11-19 21:15:52 -0800

    HTC-159: created new genericInput component, with test and snapshot

---
+++ client/src/common/forms/GenericInput.js
@@ -0,0 +1,31 @@
+/**
+ * @Author:     Parsa Rajabi
+ * @Created:    2020.11.19
+ *
+ * @Description: Generic Input
+ *
+ */
+
+import React from 'react';
+import PropTypes from "prop-types";
+
+function GenericInput(props) {
+    const {className, inputType, onChange} = props;
+    return (
+
+        <label>
+            {props.label}
+            <input className={props.className} type={props.inputType} placeholder={props.placeholder} onChange={onChange}/>
+        </label>
+    );
+}
+
+GenericInput.propTypes = {
+    className: PropTypes.string,
+    label: PropTypes.string.isRequired,
+    inputType: PropTypes.string.isRequired,
+    placeholder: PropTypes.string,
+    onChange: PropTypes.func
+};
+
+export default GenericInput;
