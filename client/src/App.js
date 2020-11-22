1ce9a9b0ad7972fa078e579d1b4cbee26411fde9
QinAlex <qinzhenyu1123@gmail.com>
2020-11-21 17:48:47 -0800

    Add Main Landing page into the Home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,36 @@
+
+
 import './App.css';
 import React from 'react'
 import Header from './common/header-and-footer/Header';
 import Home from './home/Home';
 import Footer from './common/header-and-footer/Footer';
 import Login from './login/LoginForm';
+import MainLandingPage from "./registration/MainLandingPage";
+import MemberRegistrationForm from "./registration/MemberRegistrationForm";
 import BusinessRegistration from './registration/BusinessRegistrationForm';
 import Error404 from './common/error/Error404'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
     return (
-        <Router>
-            <div>
-                <Header/>
-                {/* Matches the URL to the path and does not go through the rest of the routes*/}
-                <Switch>
-                    {/* Renders the correct component based on the URL*/}
-                    <Route path={"/"} exact component={Home}/>
-                    <Route path={"/login"} component={Login}/>
-                    <Route path={"/registration/business"} component={BusinessRegistration}/>
-                    <Route component={Error404} />
-                </Switch>
-                <Footer/>
-            </div>
-        </Router>
+        // <Router>
+        <div>
+            <Header/>
+            {/* Matches the URL to the path and does not go through the rest of the routes*/}
+            <Switch>
+                {/* Renders the correct component based on the URL*/}
+                <Route path={"/"} exact component={Home}/>
+                <Route path={"/login"} component={Login}/>
+                <Route path={"/registration/business"} component={BusinessRegistration}/>
+                <Route path={"/registration/member"} component={MemberRegistrationForm}/>
+                <Route path={"/registration"} component={MainLandingPage}/>
+                <Route component={Error404} />
+            </Switch>
+            <Footer/>
+        </div>
+        // </Router>
     );
 }
 
-export default App;
+export default App
