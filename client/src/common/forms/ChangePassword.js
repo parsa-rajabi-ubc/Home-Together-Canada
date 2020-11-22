e613f146b9603f46ee2edf9172d016a58ea21d14
lightbrite88 <lightbrite88@gmail.com>
2020-11-21 21:41:03 -0700

    HTC-258: Created new form component with snapshot test and snapshot

---
+++ client/src/common/forms/ChangePassword.js
@@ -0,0 +1,32 @@
+/**
+ * @Author:     Jeff Hatton
+ * @Created:    2020.11.19
+ *
+ * @Description: Change Password input component - returns 3 password text area inputs and a submit button.
+ *
+ */
+import React, {useState} from 'react';
+import PropTypes from "prop-types";
+import GenericInput from "./GenericInput";
+
+function ChangePassword(props) {
+    const {className, onChange, oldPassword, label} = props;
+    const [newPassword, setNewPassword] = useState("");
+    const [confirmNewPassword, setConfirmNewPassword] = useState("");
+    return (
+        <label>
+            {label}
+            <GenericInput className={className} label="Old Password: " inputType="password" placeholder={oldPassword}/>
+            <GenericInput className={className} label="New Password: " inputType="password" onChange={(e)=>{setNewPassword(e.target.value)}}/>
+            <GenericInput className={className} label="Confirm New Password: " inputType="password" onChange={(e)=>{setConfirmNewPassword(e.target.value)}}/>
+            <GenericInput className={className} label="Submit: " inputType="submit" onClick={onChange}/>
+        </label>
+    );
+}
+ChangePassword.propTypes = {
+    label: PropTypes.string.isRequired,
+    oldPassword: PropTypes.string.isRequired,
+    className: PropTypes.string,
+    onChange: PropTypes.func.isRequired
+};
+export default ChangePassword;
