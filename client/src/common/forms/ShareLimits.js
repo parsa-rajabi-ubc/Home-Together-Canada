b71b21b0b8b03e62ad72c6a79c58f2618f4e8f66
Parsa <ParsaRajabiPR@gmail.com>
2020-11-22 18:26:47 -0800

    HTC-264: added dropdown to ShareLimit.js

--- client/src/common/forms/ShareLimits.js
+++ client/src/common/forms/ShareLimits.js
@@ -6,26 +6,48 @@
  *
  */
 
+import React, { useState } from 'react';
+import Select from "react-select";
+
 const shareLimits = [
     {
-        label: "1",
+        label: "1 other person",
         value: "1"
     },
     {
-        label: "2",
+        label: "2 other people",
         value: "2"
     },
     {
-        label: "3",
+        label: "3 other people",
         value: "3"
     },
     {
-        label: "4",
+        label: "4 other people",
         value: "4"
     },
     {
-        label: "5",
-        value: "Any number"
+        label: "5+ other people",
+        value: "5"
     }
 ]
-export default shareLimits;
+
+function ShareLimit() {
+    const [selectedLimit, setsSelectedLimit] = useState(null);
+
+    const handleStatusChange = e => {
+        setsSelectedLimit(e.value);
+    }
+
+    return (
+        <div>
+            <Select isSearchable={true}
+                    options={shareLimits} value={shareLimits.find(obj => obj.value === setsSelectedLimit)}
+                    onChange={handleStatusChange}/>
+            <div><b>Selected Limit: </b> {selectedLimit}</div>
+
+        </div>
+    )
+
+}
+export default ShareLimit;
