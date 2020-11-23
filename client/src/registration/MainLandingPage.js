c2c22877d12ef3ac42fe62b5ee9540de90408876
Parsa <ParsaRajabiPR@gmail.com>
2020-11-22 17:12:33 -0800

    HTC-215: removed buttons, reaplced div with link, added hover css

--- client/src/registration/MainLandingPage.js
+++ client/src/registration/MainLandingPage.js
@@ -15,7 +15,7 @@ function MainLandingPage() {
     return (
         <div>
             <div className="flex w-full mt-5 mb-10">
-                <div className="flex flex-col w-full pt-12 mx-10 bg-blue-200 rounded-lg shadow-2xl">
+                    <Link to={'/registration/member'} className="flex flex-col w-full pt-12 mx-10 bg-blue-200 rounded-lg shadow-2xl hover:bg-blue-400">
 
                     <img className="w-auto h-64"
                          src={member}
@@ -29,21 +29,19 @@ function MainLandingPage() {
                                 <li>Create free Home Sharing listening</li>
                                 <li>Search and message other members</li>
                                 <li>Report any suspicious activity on the site</li>
-                                <Link to={'/registration/member'}
-                                      className="flex inline-block mt-8 rounded-md shadow btn btn-blue">
-                                    Create Member Account
-                                </Link>
                             </ul>
 
                         </div>
                     </div>
 
-                </div>
-                <div className="flex flex-col w-full pt-2 mx-10 bg-red-200 rounded-lg shadow-2xl">
+                </Link>
+                    <Link to={'/registration/business'} className="flex flex-col w-full pt-2 mx-10 bg-red-200 rounded-lg shadow-2xl hover:bg-red-400 ">
+
                     <img className="w-auto h-64 mt-10"
                          src={business}
                          alt="member picture">
                     </img>
+
                     <div className="m-10 mb-2 text-center text-gray-800">
                         <p className="mb-2 text-3xl font-bold leading-none text-gray-800">Business Account</p>
                         <div className="text-center">
@@ -52,16 +50,10 @@ function MainLandingPage() {
                                 <li>Create free service listening</li>
                                 <li>Create paid classified listening</li>
                                 <li>Report any suspicious activity on the site</li>
-                                <Link to={'/registration/business'}
-                                      className="flex inline-block mt-8 rounded-md shadow btn btn-red">
-                                    Create Business Account
-                                </Link>
                             </ul>
-
                         </div>
                     </div>
-
-                </div>
+                </Link>
             </div>
         </div>
     );
