8f4977edb259c0ec5dcd4a56f75eaf2338a82701
Parsa <ParsaRajabiPR@gmail.com>
2020-11-22 18:55:17 -0800

    HTC-264: removed KM from radii value

--- client/src/common/forms/Radii.js
+++ client/src/common/forms/Radii.js
@@ -9,43 +9,43 @@
 const radii = [
     {
         label: "1km",
-        value: "1km"
+        value: "1"
     },
     {
         label: "3km",
-        value: "3km"
+        value: "3"
     },
     {
         label: "5km",
-        value: "5km"
+        value: "5"
     },
     {
         label: "10km",
-        value: "10km"
+        value: "10"
     },
     {
         label: "25km",
-        value: "25km"
+        value: "25"
     },
     {
         label: "50km",
-        value: "50km"
+        value: "50"
     },
     {
         label: "75km",
-        value: "75km"
+        value: "75"
     },
     {
         label: "100km",
-        value: "100km"
+        value: "100"
     },
     {
         label: "150km",
-        value: "150km"
+        value: "150"
     },
     {
         label: "200km",
-        value: "200km"
+        value: "200"
     }
 ]
 export default radii;
