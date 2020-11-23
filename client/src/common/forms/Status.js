b15f6dceeed8457a0576a273e20b60c43a02caca
Parsa <ParsaRajabiPR@gmail.com>
2020-11-22 18:27:00 -0800

    HTC-264: added dropdown to Status.js

--- client/src/common/forms/Status.js
+++ client/src/common/forms/Status.js
@@ -1,3 +1,6 @@
+import React, { useState } from 'react';
+import Select from "react-select";
+import TextArea from "./TextArea";
 /**
  * @Author:     Jeff Hatton
  * @Created:    2020.11.18
@@ -5,6 +8,7 @@
  * @Description: Const array for Dropdown Component
  *
  */
+
 
 const statuses = [
     {
@@ -28,4 +32,27 @@ const statuses = [
         value: "Existing Group"
     }
 ]
-export default statuses;
+
+function Status() {
+    const [selectedStatus, setsSelectedStatus] = useState(null);
+
+    const handleStatusChange = e => {
+        setsSelectedStatus(e.value);
+    }
+
+    return (
+        <div>
+            <Select isSearchable={true}
+                    options={statuses} value={statuses.find(obj => obj.value === setsSelectedStatus)}
+                    onChange={handleStatusChange}/>
+            {/*{(selectedStatus === ("Couple") &&*/}
+            {/*    <TextArea label="Partner's username(s): " placeholder="Partner's username" disabled={!(status=="Couple")} onChange={(e)=>{setPartners(e.target.value)}}/>}*/}
+            {/*    /!*<TextArea label="Group members: " placeholder="usernames (separated by comma)" disabled={!(status=="Existing Group")} onChange={(e)=>{setPartners(e.target.value)}}/>}*!/*/}
+
+            {/*        <div><b>Selected Status: </b> {selectedStatus}</div>*/}
+
+        </div>
+    )
+
+}
+export default Status;
