19474858873d17e9757509a2d0c5ca7f1db9da11
Parsa Rajabi <ParsaRajabiPR@gmail.com>
2020-11-23 22:15:37 -0800

    HTC-215: MainLandingPage CSS (#267)
    * HTC-215: MainLandingPage CSS + refactor code + added svg for member + added blue button
    
    * HTC-215: updated account features list
    
    * HTC-215: removed buttons, reaplced div with link, added hover css

--- client/src/registration/MainLandingPage.js
+++ client/src/registration/MainLandingPage.js
@@ -8,34 +8,55 @@
 
 import React from 'react';
 import {Link} from "react-router-dom";
+import member from "../images/member.svg";
+import business from "../images/business.svg";
 
-function MainLandingPage(){
-    return(
+function MainLandingPage() {
+    return (
         <div>
-            <h1>Main Landing Page</h1>
-            <label>Member Account:</label> <br/>
-            * Free account with access to all features <br/>
-            * Ability to search for other members <br/>
-            * Create free service listening <br/>
-            * View other members&apos; profiles <br/>
-            * Message other members through the in-site messaging system <br/>
-            * Report any suspicious activity on the site <br/>
-            <Link to={"/registration/member"}
-                  className="text-lg font-bold lg:text-xl">
-                Member Sign Up
-            </Link>
+            <div className="flex w-full mt-5 mb-10">
+                    <Link to={'/registration/member'} className="flex flex-col w-full pt-12 mx-10 bg-blue-200 rounded-lg shadow-2xl hover:bg-blue-400">
 
-            <hr/>
-            <label>Business Account:</label> <br/>
-            * Free account with access to limited  <br/>
-            * Create free service listening <br/>
-            * Create paid classified listening <br/>
-            * Report any suspicious activity on the site <br/>
-            <Link to={"/registration/business"}
-                  className="text-lg font-bold lg:text-xl">
-                Business Sign Up
-            </Link>
+                    <img className="w-auto h-64"
+                         src={member}
+                         alt="member picture">
+                    </img>
+                    <div className="m-10 mb-2 text-center text-gray-800">
+                        <p className="mb-2 text-3xl font-bold leading-none text-gray-800">Member Account</p>
+                        <div className="mb-10 text-center">
+                            <ul className="inline-block my-3 text-lg text-left list-disc list-inside">
+                                <li>Free account registration</li>
+                                <li>Create free Home Sharing listening</li>
+                                <li>Search and message other members</li>
+                                <li>Report any suspicious activity on the site</li>
+                            </ul>
+
+                        </div>
+                    </div>
+
+                </Link>
+                    <Link to={'/registration/business'} className="flex flex-col w-full pt-2 mx-10 bg-red-200 rounded-lg shadow-2xl hover:bg-red-400 ">
+
+                    <img className="w-auto h-64 mt-10"
+                         src={business}
+                         alt="member picture">
+                    </img>
+
+                    <div className="m-10 mb-2 text-center text-gray-800">
+                        <p className="mb-2 text-3xl font-bold leading-none text-gray-800">Business Account</p>
+                        <div className="text-center">
+                            <ul className="inline-block my-3 text-lg text-left list-disc list-inside">
+                                <li>Free account registration</li>
+                                <li>Create free service listening</li>
+                                <li>Create paid classified listening</li>
+                                <li>Report any suspicious activity on the site</li>
+                            </ul>
+                        </div>
+                    </div>
+                </Link>
+            </div>
         </div>
-    )
+    );
 }
+
 export default MainLandingPage;
