7a5e4fadb9069611d2d8e1b3a5dec3480183e712
Parsa <ParsaRajabiPR@gmail.com>
2020-11-24 01:45:50 -0800

    Updated snapshots

--- client/src/registration/__tests__/MemberProfileForm.test.js
+++ client/src/registration/__tests__/MemberProfileForm.test.js
@@ -8,12 +8,18 @@
 import React from 'react';
 import renderer from  'react-test-renderer'
 import MemberProfileForm from "../MemberProfileForm";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 describe('MemberProfileForm', () => {
     describe('Snapshot test', () => {
         it("should render correctly regardless of properties", () => {
+            const MemberProfile = (
+                <Router>
+                    <MemberProfileForm />
+                </Router>
+            )
             //when
-            const component = renderer.create(<MemberProfileForm/>).toJSON();
+            const component = renderer.create(MemberProfile).toJSON();
             //then
             expect(component).toMatchSnapshot();
         });
