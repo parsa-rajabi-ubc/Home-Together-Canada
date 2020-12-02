919c89f6450a8a14fef63c6ee8117023a63d48a0
Parsa <ParsaRajabiPR@gmail.com>
2020-12-01 17:35:46 -0800

    HTC-299: modified mainlandingPage test to pass

--- client/src/registration/__tests__/MainLandingPage.test.js
+++ client/src/registration/__tests__/MainLandingPage.test.js
@@ -9,6 +9,8 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import {BrowserRouter as Router} from 'react-router-dom';
 import MainLandingPage from "../MainLandingPage";
+import member from '../../images/member.svg';
+import business from '../../images/business.svg';
 
 describe('MainLandingPage', () => {
     describe('Snapshot test', () => {
@@ -18,9 +20,12 @@ describe('MainLandingPage', () => {
                     <MainLandingPage/>
                 </Router>
             );
+
             //when
             const component = renderer.create(RegistrationLandingPage).toJSON();
             //then
+            expect(member).toEqual('member.svg');
+            expect(business).toEqual('business.svg');
             expect(component).toMatchSnapshot();
         });
     })
