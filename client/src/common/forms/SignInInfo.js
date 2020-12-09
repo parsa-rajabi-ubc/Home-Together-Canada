f009e7379d16ff00c1b321a79326f1b275e17c86
Parsa <ParsaRajabiPR@gmail.com>
2020-12-08 23:22:21 -0800

    HTC-343: added tooltip to SignInInfo component

--- client/src/common/forms/SignInInfo.js
+++ client/src/common/forms/SignInInfo.js
@@ -8,21 +8,32 @@
 
 import React from "react";
 import PropTypes from "prop-types";
+import Tooltip from "./Tooltip";
+
+const infoText = {
+    username: "Your username should be unique and meaningful. Avoid using numbers in your username!",
+    password: "Your password must be at least 8 characters long and contain a mix of numeric digits, upper and lower case letters!",
+    confirmPassword: "This password must match the password you entered above!"
+};
+
 
 function SignInInfo(props){
     const { onChangeUsername, onChangePassword, onChangePasswordCheck } = props;
     return(
         <div>
-            <label className={"label"}>
-                Username:
+            <label>
+                <span className={"label"}>Username</span>
+                <Tooltip text={infoText["username"]} toolTipID="username"/>
                 <input className={"input"} type="text" placeholder="" onChange= {onChangeUsername}/>
             </label>
-            <label className={"label"}>
-                Password:
+            <label>
+                <span className={"label"}>Password</span>
+                <Tooltip text={infoText["password"]} toolTipID="password"/>
                 <input className={"input"} type="password" placeholder="" onChange= {onChangePassword}/>
             </label>
-            <label className={"label"}>
-                Confirm Password:
+            <label>
+                <span className={"label"}>Confirm Password</span>
+                <Tooltip text={infoText["confirmPassword"]} toolTipID="confirmPassword"/>
                 <input className={"input"} type="password" placeholder="" onChange= {onChangePasswordCheck}/>
             </label>
         </div>
