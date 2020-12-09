24b21a254d17f715d6a4bd1a7eb990c5e8cebfd5
Parsa <ParsaRajabiPR@gmail.com>
2020-12-09 12:21:02 -0800

    HTC-343: cleaned up tooltip.test

--- client/src/common/forms/__tests__/Tooltip.test.js
+++ client/src/common/forms/__tests__/Tooltip.test.js
@@ -7,22 +7,19 @@
  */
 
 import React from 'react';
-import renderer from  'react-test-renderer'
+import renderer from 'react-test-renderer'
 import Tooltip from "../Tooltip";
-jest.mock("react-tooltip/node_modules/uuid", () => ({ v4: () => "00000000-0000-0000-0000-000000000000" }));
 
+jest.mock("react-tooltip/node_modules/uuid", () => ({v4: () => "00000000-0000-0000-0000-000000000000"}));
 
 describe('Tooltip', () => {
     describe('Snapshot test', () => {
         it("should render correctly regardless of properties", () => {
-
-            const generateClassName = (rule, styleSheet) =>
-                `${styleSheet.options.classNamePrefix}-${rule.key}`;
             // given
             const text = "helpful text";
             const toolTipID = "randomID";
             //when
-            const component = renderer.create(<Tooltip text={text} toolTipID={toolTipID} />).toJSON();
+            const component = renderer.create(<Tooltip text={text} toolTipID={toolTipID}/>).toJSON();
             //then
             expect(component).toMatchSnapshot();
         });
