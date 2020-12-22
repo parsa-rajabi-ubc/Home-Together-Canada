9c99ac8135c2451973204b759ef5d7bee45e9b98
Parsa <ParsaRajabiPR@gmail.com>
2020-12-21 18:43:57 -0800

    HTC-343: updated family_status tooltip

--- client/src/registration/MemberRegistrationForm.js
+++ client/src/registration/MemberRegistrationForm.js
@@ -252,7 +252,7 @@ function MemberRegistrationForm(props) {
     const INFO_TEXT = {
         YEAR_OF_BIRTH: "Age is required in order to help members connect with others in their desired age range",
         DIFF_MAILING_ADDRESS: "Select this checkbox if your mailing address differs from the address above",
-        FAMILY_STATUS: "Select the status that most accurately reflects your living arrangement that you would like to maintain while home-sharing with",
+        FAMILY_STATUS: "Select the status that most accurately reflects your living arrangement that you would like to maintain while home-sharing",
         NUM_PEOPLE_SHARE: "This number of people does not include yourself",
         RENT: "Please list your minimum and maximum budget for rent. Note: This does NOT include utilities",
         INTERESTED_AREA: "Preferred living location(s) across Canada",
