9e251036070d32268adaacc9b0f9dae5c5c295f2
Parsa <ParsaRajabiPR@gmail.com>
2021-01-03 17:37:56 -0800

    HTC-291: removed unused code

--- client/src/common/forms/InterestedArea.js
+++ client/src/common/forms/InterestedArea.js
@@ -49,78 +49,61 @@ function InterestedArea(props) {
         list[index].radius = value;
         setExtraAreas(list);
     };
-    const error = [];
     useEffect(() => {
         onChange(
             extraAreas
         )
-        validator();
     }, [extraAreas]);
 
-    const validator = () => {
-        for (let i = 0; i <= extraAreas.length - 1; i++) {
-            if (extraAreas[i].province === undefined) {
-                console.log("prov at index", i)
-                error.push(i);
-            } else if (extraAreas[i].city === undefined) {
-                console.log("city at index", i)
-                error.push(i);
-            } else if (extraAreas[i].radius === undefined) {
-                console.log("radius at index", i)
-                error.push(i);
-            }
-        }
-    }
-        console.log("error is OUTSIDE", error);
     return (
         <div>
             <section className={`${areasOfInterestError && "border rounded-lg p-1 border-red-500"}`}>
 
                 {extraAreas.map((currentAreaValues, index) => {
-                return (
-                    <div key={index} className="grid grid-cols-9 gap-x-2">
+                    return (
+                        <div key={index} className="grid grid-cols-9 gap-x-2">
 
-                        <div className="col-start-1 col-end-4">
-                            <Dropdown isSearchable={true} placeholder={"Province"}
-                                      name="province"
-                                      options={getProvinces()}
-                                      onChange={e => handleAreaProvinceChange(e, index)}
-                                      styling={styling}
-                            />
-                            <span>{currentAreaValues[index]}</span>
-                        </div>
-                        <div className="col-start-4 col-end-8">
-                            {currentAreaValues.province &&
-                            <Dropdown isSearchable={true} placeholder={"City"}
-                                      name="city"
-                                      options={getCities(currentAreaValues.province)}
-                                      onChange={e => handleAreaCityChange(e, index)}
-                                      styling={styling}
-                            />}
-                        </div>
-                        <div className="col-start-8 col-end-10">
-                            {currentAreaValues.city && <Dropdown isSearchable={true} placeholder={"Within Radius"}
-                                                                 name="radius"
-                                                                 options={radii}
-                                                                 onChange={e => handleAreaRadiusChange(e, index)}
-                                                                 styling={styling}
-                            />}
-                        </div>
+                            <div className="col-start-1 col-end-4">
+                                <Dropdown isSearchable={true} placeholder={"Province"}
+                                          name="province"
+                                          options={getProvinces()}
+                                          onChange={e => handleAreaProvinceChange(e, index)}
+                                          styling={styling}
+                                />
+                                <span>{currentAreaValues[index]}</span>
+                            </div>
+                            <div className="col-start-4 col-end-8">
+                                {currentAreaValues.province &&
+                                <Dropdown isSearchable={true} placeholder={"City"}
+                                          name="city"
+                                          options={getCities(currentAreaValues.province)}
+                                          onChange={e => handleAreaCityChange(e, index)}
+                                          styling={styling}
+                                />}
+                            </div>
+                            <div className="col-start-8 col-end-10">
+                                {currentAreaValues.city && <Dropdown isSearchable={true} placeholder={"Within Radius"}
+                                                                     name="radius"
+                                                                     options={radii}
+                                                                     onChange={e => handleAreaRadiusChange(e, index)}
+                                                                     styling={styling}
+                                />}
+                            </div>
 
-                        <div className="col-start-10 col-end-auto">
-                            {extraAreas.length !== 1 &&
-                            <MdDeleteForever color="#DB4437" size="40" onClick={() => handleRemoveClick(index)}/>}
+                            <div className="col-start-10 col-end-auto">
+                                {extraAreas.length !== 1 &&
+                                <MdDeleteForever color="#DB4437" size="40" onClick={() => handleRemoveClick(index)}/>}
+                            </div>
+                            <div className="col-start-1 col-end-4">
+                                {extraAreas.length - 1 === index &&
+                                <Button className="btn btn-green text-sm py-2 " value="Add Another Location"
+                                        onClick={() => {
+                                            handleAddClick()
+                                        }}/>}
+                            </div>
                         </div>
-                        <div className="col-start-1 col-end-4">
-                            {extraAreas.length - 1 === index &&
-                            <Button className="btn btn-green text-sm py-2 " value="Add Another Location"
-                                    onClick={() => {
-                                        handleAddClick()
-                                    }}/>}
-                        </div>
-                    </div>
-                );
-            })}
+                    );
+                })}
             </section>
             <div className="col-start-1 col-end-6">
                 {areaOfInterestErrorMsg && <label className={"error-msg"}>{areaOfInterestErrorMsg}</label>}
