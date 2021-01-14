8fb9e99df30f508c21334719b8e723bb7eb500a5
Parsa <ParsaRajabiPR@gmail.com>
2021-01-14 14:32:25 -0800

    HTC-418: Created Header.css, updated header names, updated header css, added header.css to component.css

--- client/src/common/header-and-footer/Header.js
+++ client/src/common/header-and-footer/Header.js
@@ -23,7 +23,6 @@ import {dropdownAccountCSS, dropdownAccountTheme} from "../../css/dropdownCSSUti
 const mapDispatch = {setIsAdmin, setAccountType, setAuthenticated};
 
 
-
 const Header = (props) => {
 
     const {
@@ -55,9 +54,9 @@ const Header = (props) => {
 
     return (
         <div>
-            {/*TODO: change bg color to igreen*/}
             <nav className="top-0 flex w-full bg-green-400 tex-black">
-                <div className="container flex items-center justify-between w-full py-2 mx-auto mt-0 whitespace-no-wrap">
+                <div
+                    className="container flex items-center justify-between w-full py-2 mx-auto mt-0 whitespace-no-wrap">
                     <div className="flex flex-no-wrap items-center ml-4">
                         <Link to={'/'}
                               className="text-lg font-bold lg:text-xl">
@@ -65,42 +64,34 @@ const Header = (props) => {
                         </Link>
                     </div>
 
-                    {/*TODO: move classNames to header.css */}
                     {/* Middle of Nav */}
                     <div
                         className="flex-no-wrap hidden w-full p-4 mt-2 tex-black lg:flex lg:items-center lg:w-auto lg:block lg:mt-0 lg:bg-transparent lg:p-0">
                         <div className="items-center justify-end flex-1 mr-16 list-reset lg:flex">
                             {accountType !== USER_TYPES.BUSINESS &&
-                            <Link to={'/members'}
-                                  className="inline-block px-4 py-2 mr-3 transition duration-200 ease-in-out border-transparent rounded-md hover:bg-white hover:text-black hover:opacity-50">
+                            <Link to={'/members'} className="nav-icon">
                                 Search Member Profiles
                             </Link>
                             }
-                            <Link to={'/services'}
-                                  className="inline-block px-4 py-2 mr-3 transition duration-200 ease-in-out border-transparent rounded-md hover:bg-white hover:text-black hover:opacity-50">
-                                Services
+                            <Link to={'/services'} className="nav-icon">
+                                Home Sharing Services
                             </Link>
-                            <Link to={'/classifieds'}
-                                  className="inline-block px-4 py-2 mr-3 transition duration-200 ease-in-out border-transparent rounded-md hover:bg-white hover:text-black hover:opacity-50">
-                                Classifieds
+                            <Link to={'/classifieds'} className="nav-icon">
+                                Local Classifieds
                             </Link>
-                            <Link to={'/about'}
-                                  className="inline-block px-4 py-2 mr-3 transition duration-200 ease-in-out border-transparent rounded-md hover:bg-white hover:text-black hover:opacity-50">
+                            <Link to={'/about'} className="nav-icon">
                                 About Us
                             </Link>
-                            <Link to={'/faq'}
-                                  className="inline-block px-4 py-2 mr-3 no-underline transition duration-200 ease-in-out border-transparent rounded-md hover:bg-white hover:text-black hover:opacity-50">
+                            <Link to={'/faq'} className="nav-icon">
                                 FAQ
                             </Link>
                             {(authenticated && accountType !== USER_TYPES.UNREGISTERED) &&
-                            < Link to={'/create/listing'}
-                                   className="inline-block px-4 py-2 mr-3 no-underline transition duration-200 ease-in-out border-transparent rounded-md hover:bg-white hover:text-black hover:opacity-50">
+                            < Link to={'/create/listing'} className="nav-icon">
                                 Create Listing
                             </Link>
                             }
                             {(authenticated && isAdmin) &&
-                            <Link to={'/admin'}
-                                  className="inline-block px-4 py-2 mr-3 no-underline transition duration-200 ease-in-out border-transparent rounded-md hover:bg-white hover:text-black hover:opacity-50">
+                            <Link to={'/admin'} className="nav-icon">
                                 Admin
                             </Link>
                             }
@@ -108,14 +99,13 @@ const Header = (props) => {
 
                         {/* Login and Sign Up Buttons */}
                         {(accountType === USER_TYPES.UNREGISTERED || !authenticated) &&
-                        <Link to={'/login'}
-                              className="items-center justify-center w-full px-6 py-2 mr-4 transition duration-200 ease-in-out bg-white border-transparent rounded-md opacity-75 hover:bg-orange-400">Login
+                        <Link to={'/login'} className="nav-btn">
+                            Login
                         </Link>
                         }
                         {(accountType === USER_TYPES.UNREGISTERED || !authenticated) &&
-                        <Link to={'/registration'}
-                              className="items-center justify-center w-full px-4 py-2 mr-4 transition duration-200 ease-in-out bg-white border-transparent rounded-md opacity-75 hover:bg-orange-400">Sign
-                            Up
+                        <Link to={'/registration'} className="nav-btn">
+                            Sign Up
                         </Link>
                         }
                     </div>
