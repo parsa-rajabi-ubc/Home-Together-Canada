b64e0f2c5eac10d7edf974cc99b9ede8d9b8d683
Parsa <ParsaRajabiPR@gmail.com>
2021-01-20 00:14:58 -0800

    HTC-389: Updated snapshots

--- client/src/registration/__tests__/BusinessRegistrationForm.test.js
+++ client/src/registration/__tests__/BusinessRegistrationForm.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import renderer from 'react-test-renderer'
 import BusinessRegistrationForm from "../BusinessRegistrationForm";
+import { BrowserRouter as Router } from 'react-router-dom';
+import MemberRegistrationForm from "../MemberRegistrationForm";
+
 
 jest.mock("react-tooltip/node_modules/uuid", () => ({
     v4: () => "00000000-0000-0000-0000-000000000000"}
@@ -26,7 +29,7 @@ describe('BusinessRegistrationForm', () => {
             setAuthenticated
         };
         //when
-        const component = renderer.create(<BusinessRegistrationForm {...props}/>);
+        const component = renderer.create(<Router><BusinessRegistrationForm {...props}/></Router>);
         const tree = component.toJSON();
         //then
         expect(tree).toMatchSnapshot();
