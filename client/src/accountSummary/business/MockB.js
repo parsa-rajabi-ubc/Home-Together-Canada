010cc95e913b679bda2929500186cd627bead3d8
QinAlex <43220166+QinAlex@users.noreply.github.com>
2021-01-20 17:09:52 -0800

    HTC-286: Business Account Summary
    * HTC-286/Business Account Summary
    
    * HTC-286/Business Account Summary

---
+++ client/src/accountSummary/business/MockB.js
@@ -0,0 +1,42 @@
+export const businessUser = {
+    logo: "uploaded file",
+    bName : "Sal's Homesharing Service",
+    username : "BigApple",
+    bEmail: "sal@homesharing.com",
+    useDifferentMailingAddress : true,
+    isIncorporated : true,
+    incorporatedOwnersNames : "Bob",
+    isNationWide : false,
+    website : "www.bigApple.com",
+    firstName: "Jack",
+    lastName: "Lee",
+    bPhoneNumber : "778-821-5555",
+    bCellNumber : "250-666-8888",
+    personalEmail : "nao@gmail.com",
+    contactPhoneNumber : "778-565-6565",
+    bAddress: {
+        street: "766 Rutland Rd. S.",
+        aptNum: "#9",
+        city: "Kelowna",
+        province: "BC",
+        postalCode: "V1X 5B5"
+    },
+    bMailingAddress: {
+        street: "333 kkk Rd",
+        aptNum: "#3",
+        city: "Fat Rose",
+        province: "BC",
+        postalCode: "V1X 3B3"
+    },
+    bMapAddress: {
+        street: "555 haha Rd",
+        aptNum: "#5",
+        city: "Cool Jack",
+        province: "BC",
+        postalCode: "V1X 5B5"
+    },
+    contactFName:"Lee",
+    contactLName:"Jeck",
+    password:"123",
+    passwordCheck:"123"
+}
