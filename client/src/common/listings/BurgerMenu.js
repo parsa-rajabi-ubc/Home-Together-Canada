34f134244346f8ee1a4090e79fa98ef71cbb18a8
Parsa Rajabi <ParsaRajabiPR@gmail.com>
2021-01-25 22:05:58 -0800

    HTC-367: Burger Menu
    * HTC-367: Added filter btn
    
    * HTC-367: Installed react-menu-bar and react-remove-scroll
    
    * HTC-367: Implemented Menu, NoScroll, Updated filter-btn
    
    * HTC-367: Added TODO
    
    * HTC-367: Created BurgetMenu Component, Test
    
    * HTC-367: Added snapshot
    
    * HTC-367: Added filter btn
    
    * HTC-367: Installed react-menu-bar and react-remove-scroll
    
    * HTC-367: Implemented Menu, NoScroll, Updated filter-btn
    
    * HTC-367: Added TODO
    
    * HTC-367: Created BurgetMenu Component, Test
    
    * HTC-367: Added snapshot
    
    * HTC-367: Attempting to fix test failures
    
    * HTC-367: Updated content prop type

---
+++ client/src/common/listings/BurgerMenu.js
@@ -0,0 +1,39 @@
+/**
+ * @Author:     Parsa Rajabi
+ * @Created:    2021.1.25
+ *
+ * @Description: Reusable Menu component
+ *
+ */
+import React from 'react';
+import PropTypes from "prop-types";
+import Menu from 'react-burger-menu/lib/menus/slide';
+
+
+const BurgerMenu = (props) => {
+    const {isOpen, onClose, content} = props;
+
+    return (
+        //https://github.com/negomi/react-burger-menu
+        <Menu
+            width={"30%"}
+            disableAutoFocus
+            itemListElement="div"
+            customBurgerIcon={false}
+            customCrossIcon={false}
+            isOpen={isOpen} onClose={onClose}
+            menuClassName={"bg-gray-700 p-5 rounded-xl"}
+            itemListClassName={"p-2 bg-gray-700"}
+        >
+            {content}
+        </Menu>
+    )
+}
+
+BurgerMenu.propTypes = {
+    isOpen: PropTypes.bool,
+    onClose: PropTypes.func,
+    content: PropTypes.element
+};
+
+export default BurgerMenu;
