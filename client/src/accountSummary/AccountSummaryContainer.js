88808789f4f81440461df48921b92782525cd0d7
QinAlex <qinzhenyu1123@gmail.com>
2021-01-26 17:17:20 -0800

    HTC-237:Deactivate/Delete-Account

--- client/src/accountSummary/AccountSummaryContainer.js
+++ client/src/accountSummary/AccountSummaryContainer.js
@@ -16,6 +16,8 @@ import MemberProfileSummaryContainer from "./member/MemberProfileSummaryContaine
 import BusinessAccountSummaryContainer from "./business/BusinessAccountSummaryContainer";
 import Error404 from "../common/error/Error404";
 import SearchCriteriaContainer from "./member/SearchCriteriaContainer";
+import DeactivateAccount from "./accountDeactivateAndDelete/DeactivateAccount";
+import DeleteAccount from "./accountDeactivateAndDelete/DeleteAccount"
 
 const AccountSummaryContainer = () => {
     const {accountType, selected} = useLocation().state;
@@ -38,6 +40,10 @@ const AccountSummaryContainer = () => {
                 return <div>Messaging Component</div>
             case ALL_SUBPAGES.MANAGE_LISTINGS:
                 return <div>Manage Listings Component</div>
+            case ALL_SUBPAGES.ACTIVATE_DEACTIVATE:
+                return accountType === USER_TYPES.MEMBER ? <DeactivateAccount memberAccountInfo ={memberAccountInfo}/> : <Error404/>
+            case ALL_SUBPAGES.DELETE:
+                return <DeleteAccount accountInfo = {memberAccountInfo}/>
             default:
                 return accountType === USER_TYPES.MEMBER
                     ? <MemberAccountSummaryContainer/>
