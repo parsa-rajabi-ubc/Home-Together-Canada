c440d22e847f2809e7e48fea61e9c22540ca801b
Parsa <ParsaRajabiPR@gmail.com>
2021-01-30 19:38:15 -0800

    HTC-501: re-formatted Search Result Container

--- client/src/memberSearch/SearchResultsContainer.js
+++ client/src/memberSearch/SearchResultsContainer.js
@@ -17,10 +17,10 @@ const START_INDEX = 0;
 
 function SearchResultsContainer(props) {
     const {profileData} = props;
+
     //I left this as a state variable since it will inevitably be needed as one
-    const [filteredResults, setFilteredResults] = useState(profileData.length && profileData.length >= NUM_RESULTS
-        ? limitResults(profileData, NUM_RESULTS, START_INDEX)
-        : limitResults(profileData, profileData.length, START_INDEX));
+    const [filteredResults, setFilteredResults] = useState(
+        profileData.length && profileData.length >= NUM_RESULTS ? limitResults(profileData, NUM_RESULTS, START_INDEX) : limitResults(profileData, profileData.length, START_INDEX));
 
     return (
         <div>
