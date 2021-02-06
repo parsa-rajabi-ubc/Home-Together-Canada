73c19acd09c4c9c7c69cee487e37da0de9402cc8
Parsa <ParsaRajabiPR@gmail.com>
2021-02-05 21:29:37 -0800

    HTC-440: Added Listing Constants to Common/Constants

---
+++ client/src/common/constants/listingsConstants.js
@@ -0,0 +1,12 @@
+/**
+ * @Author:     Parsa Rajabi
+ * @Created:    2021.02.04
+ *
+ * @Description: Constants for Listings
+ *
+ */
+
+
+export const CREATE_LISTINGS_TEXT = "Create Listing"
+export const SERVICES_TEXT = "Home Sharing Services";
+export const CLASSIFIEDS_TEXT = "Local Classifieds";
