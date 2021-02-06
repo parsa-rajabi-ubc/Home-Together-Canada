b7741b21d866ed95adc470bda39187885838fcf1
Parsa <ParsaRajabiPR@gmail.com>
2021-02-05 21:27:54 -0800

    HTC-440: Updated Constants

---
+++ client/src/createListing/constants/serviceListingText.js
@@ -0,0 +1,62 @@
+/**
+ * @Author:     Parsa Rajabi
+ * @Created:    2021.02.04
+ *
+ * @Description: Constants for Services Text
+ *
+ */
+
+const OTHER = "Other";
+
+export const MEMBER_SERVICE_CATEGORIES = {
+    MEMBER_HOME: "Members with Homes to Share",
+}
+
+export const MEMBER_HOME_SHARE_SUBCATEGORIES = {
+    CITY: "City",
+    TOWN_VILLAGE: "Town/Village",
+    RURAL: "Rural",
+    COUNTRY_REMOTE: "Country/Remote",
+    OTHER: OTHER
+}
+
+export const BUSINESS_SERVICE_CATEGORIES = {
+    SHARE_COMMUNITY: "Shared & Community Living Initiatives",
+    SHARED_SERVICES: "Sharing Facilitation, Matching & Educational Services",
+    GOVERNMENT_SERVICES: "Governmental Supports and Services",
+}
+
+
+export const SHARED_COMMUNITY_SUBCATEGORIES = {
+    CO_HOUSING: "Co-housing Groups & Communities",
+    CO_OP: "Cooperatives",
+    COMMUNAL: "Communal Living",
+    ECO: "Eco villages",
+    INTER_GEN: "Intergenerational Living",
+    SHARED_LIVING: "Shared Living First Step Homes",
+    OTHER: OTHER
+}
+
+export const SHARED_SERVICES_SUBCATEGORIES = {
+    GROUP_MEET_UPS: "Facilitated Group Meet ups & Get Together",
+    SENIOR_MEET_UPS: "Facilitated Senior Meet ups & Get Together",
+    SINGLE_PARENT: "Single Parent Meet ups & Get Together",
+    MEMBER_INITIATED: "Member Initiated Meet ups & Get together",
+    HOME_CONTRACT: "Home Share Contract and Agreement Writing: workshops, webinars and Seminars",
+    HOME_SERVICES: "Home Share Matching Services",
+    STUDENT: "Senior/Student matching services",
+    NEGOTIATION: "Home Share Negotiation Services",
+    CONFLICT: "Home Share Conflict Resolution Services",
+    ONLINE: "Online Sharing Workshops",
+    HOW_TO: "How to Create a Shared Home: Workshops, Webinars or Seminars",
+    INSURANCE: "Understanding insurance when sharing a home, vehicle or other: workshops, webinars or Seminars",
+    OTHER: OTHER
+}
+
+export const GOVERNMENT_SERVICES_SUBCATEGORIES = {
+    HOUSING_FIRST: "Housing First Shared Living Services",
+    LOW_INCOME: "Low Income Shared Living Services",
+    SENIOR_HOME: "Senior Home Assistance & Health Care Services in a shared Home",
+    SPECIAL_NEEDS: "Special Needs Home Share Services",
+    OTHER: OTHER
+}
