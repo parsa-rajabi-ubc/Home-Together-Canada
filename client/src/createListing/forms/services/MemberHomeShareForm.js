d49e55e00222894e15d5614ac0df8abff9895962
Parsa <ParsaRajabiPR@gmail.com>
2021-02-05 21:27:32 -0800

    HTC-440: Added Create Listing Member Share Home, Updated Create Listing Container and Controls

---
+++ client/src/createListing/forms/services/MemberHomeShareForm.js
@@ -0,0 +1,286 @@
+/**
+ * @Author:     Parsa Rajabi
+ * @Created:    2021.2.3
+ *
+ * @Description: Member Home Share Form;
+ *
+ */
+
+import React, {useEffect, useState} from 'react';
+import PropTypes from "prop-types";
+import {MEMBER_HOME_SHARE_TEXT as Text} from "./constants/ServiceListingText";
+import {
+    checkIfErrorsExistInMapping,
+    validateCheckbox,
+    validateInput,
+} from "../../../registration/registrationUtils";
+import TextArea from "../../../common/forms/TextArea";
+import LargeTextArea from "../../../common/forms/LargeTextArea";
+import Dropdown from "../../../common/forms/Dropdown";
+import {options} from "./constants/BedroomBathroomDropdownOptions";
+import LabelAsterisk from "../../../common/forms/LabelAsterisk";
+import {dropdownDefaultCSS, dropdownErrorCSS} from "../../../css/dropdownCSSUtil";
+import YNButton from "../../../common/forms/YNButtons";
+import UploadImage from "../../../common/forms/UploadImage";
+import SubmitButton from "../../../common/forms/SubmitButton";
+import Tooltip from "../../../common/forms/Tooltip";
+import {CREATE_LISTING_MEMBER_SHARE_HOME as ToolTipText} from "../../../common/constants/TooltipText";
+
+
+const MemberHomeShareForm = (props) => {
+    const {onSubmit} = props;
+
+    const [title, setTitle] = useState(undefined);
+    const [postalCode, setPostalCode] = useState(undefined);
+    const [shortDescription, setShortDescription] = useState(undefined);
+    const [fullDescription, setFullDescription] = useState(undefined);
+    const [monthlyCost, setMonthlyCost] = useState(undefined);
+    const [utilIncluded, setUtilIncluded] = useState(undefined);
+    const [numBed, setNumBed] = useState(undefined);
+    const [numBath, setNumBath] = useState(undefined);
+    const [petFriendly, setPetFriendly] = useState(undefined);
+    const [smokeFriendly, setSmokeFriendly] = useState(undefined);
+    const [photos, setPhotos] = useState(undefined);
+
+    const [titleError, setTitleError] = useState(undefined);
+    const [postalCodeError, setPostalCodeError] = useState(undefined);
+    const [shortDescriptionError, setShortDescriptionError] = useState(undefined);
+    const [fullDescriptionError, setFullDescriptionError] = useState(undefined);
+    const [monthlyCostError, setMonthlyCostError] = useState(undefined);
+    const [utilIncludedError, setUtilIncludedError] = useState(undefined);
+    const [numBedError, setNumBedError] = useState(undefined);
+    const [numBathError, setNumBathError] = useState(undefined);
+    const [petFriendlyError, setPetFriendlyError] = useState(undefined);
+    const [smokeFriendlyError, setSmokeFriendlyError] = useState(undefined);
+
+
+    const SHORT_DESC_CHAR_COUNT = "50";
+
+    useEffect(() => {
+        title !== undefined && validateInput(title, setTitleError);
+    }, [title]);
+    useEffect(() => {
+        postalCode !== undefined && validateInput(postalCode, setPostalCodeError);
+    }, [postalCode]);
+    useEffect(() => {
+        shortDescription !== undefined && validateInput(shortDescription, setShortDescriptionError);
+    }, [shortDescription]);
+    useEffect(() => {
+        fullDescription !== undefined && validateInput(fullDescription, setFullDescriptionError);
+    }, [fullDescription]);
+    useEffect(() => {
+        monthlyCost !== undefined && validateInput(monthlyCost, setMonthlyCostError);
+    }, [monthlyCost]);
+    useEffect(() => {
+        numBed !== undefined && validateInput(numBed, setNumBedError);
+    }, [numBed]);
+    useEffect(() => {
+        numBath !== undefined && validateInput(numBath, setNumBathError);
+    }, [numBath]);
+    useEffect(() => {
+        petFriendly !== undefined && validateInput(petFriendly, setPetFriendlyError);
+    }, [petFriendly]);
+    useEffect(() => {
+        smokeFriendly !== undefined && validateInput(smokeFriendly, setSmokeFriendlyError);
+    }, [smokeFriendly]);
+    useEffect(() => {
+        utilIncluded !== undefined && validateCheckbox(utilIncluded, setUtilIncludedError);
+    }, [utilIncluded]);
+
+
+    const handleNumBedChange = e => {
+        setNumBed(e.value);
+    }
+
+    const handleNumBathChange = e => {
+        setNumBath(e.value);
+    }
+
+    function handleImageUpload(e) {
+        setPhotos(e.target.files[0]);
+    }
+
+    const isFormValid = () => {
+
+        const errors = {
+            title: false,
+            postalCode: false,
+            shortDes: false,
+            fullDes: false,
+            monthlyCost: false,
+            utilIncluded: false,
+            numBed: false,
+            numBath: false,
+            pet: false,
+            smoking: false,
+        }
+
+        errors.title = validateInput(title, setTitleError);
+        errors.postalCode = validateInput(postalCode, setPostalCodeError);
+        errors.shortDes = validateInput(shortDescription, setShortDescriptionError);
+        errors.fullDes = validateInput(fullDescription, setFullDescriptionError);
+        errors.monthlyCost = validateInput(monthlyCost, setMonthlyCostError);
+        errors.utilIncluded = validateInput(utilIncluded, setUtilIncludedError);
+        errors.numBed = validateInput(numBed, setNumBedError);
+        errors.numBath = validateInput(numBath, setNumBathError);
+        errors.pet = validateInput(petFriendly, setPetFriendlyError);
+        errors.smokeFriendly = validateInput(smokeFriendly, setSmokeFriendlyError);
+
+        return !(checkIfErrorsExistInMapping(errors));
+    }
+
+    //function for input checks on submit
+    function onCreateListing(event) {
+
+        if (!isFormValid()) {
+            console.log("Form is invalid")
+            // form is invalid
+            event.preventDefault();
+        }
+        // else form is valid and proceed to make request
+        else {
+            onSubmit();
+        }
+    }
+
+    return (
+        <div>
+            <div className="col-start-1 col-end-7 py-5 px-5 m-6 bg-white shadow-lg rounded-xl">
+                <div className="grid grid-cols-2 gap-6">
+                    <div className="col-span-3 sm:col-span-2">
+
+                        <h1 className={"page-title mb-5"}> {Text.form_title} </h1>
+
+                        <TextArea
+                            className={`${titleError && "border-red-500"} input`}
+                            label={Text.title}
+                            labelClassName={"label"}
+                            required={true}
+                            onChange={(e) => setTitle(e.target.value)}
+                        />
+
+                        <div className={"grid grid-cols-9 gap-x-6"}>
+
+                            <section className={"col-start-1 col-end-5"}>
+
+                                <TextArea
+                                    className={`${shortDescriptionError && "border-red-500"} input`}
+                                    label={Text.short_des + " (" + SHORT_DESC_CHAR_COUNT + " Characters)"}
+                                    labelClassName={"label"}
+                                    required={true}
+                                    onChange={(e) => setShortDescription(e.target.value)}
+                                    charLimit={SHORT_DESC_CHAR_COUNT}
+                                />
+                            </section>
+                            <section className={"col-start-6 col-end-9"}>
+                                <TextArea
+                                    className={`${postalCodeError && "border-red-500"} input`}
+                                    label={Text.postal_code}
+                                    labelClassName={"label"}
+                                    required={true}
+                                    onChange={(e) => setPostalCode(e.target.value)}
+                                />
+                            </section>
+
+
+                            <section className={"col-start-1 col-end-5"}>
+                                <LabelAsterisk label={Text.monthly_cost} className={"label"}/>
+                                <input
+                                    className={`${monthlyCostError && "border-red-500"} input`}
+                                    type="number"
+                                    min="0"
+                                    step="1"
+                                    placeholder=""
+                                    onChange={(e) => setMonthlyCost(e.target.value)}
+                                />
+                            </section>
+                            <section
+                                className={`${utilIncludedError && "pl-1 border rounded-lg border-red-500"} my-2 col-start-6 col-end-9`}>
+                                <YNButton
+                                    label={Text.util_included}
+                                    name="utilIncluded"
+                                    required={true}
+                                    value={utilIncluded}
+                                    onChange={(e) => setUtilIncluded(e.target.value)}
+                                />
+                            </section>
+                            <section className={"col-start-1 col-end-5"}>
+                                <LabelAsterisk
+                                    label={Text.num_bed}
+                                    className={"label"}
+                                />
+
+                                <Dropdown
+                                    options={options}
+                                    onChange={handleNumBedChange}
+                                    dropdownCSS={numBedError ? dropdownErrorCSS : dropdownDefaultCSS}/>
+
+                            </section>
+
+                            <section
+                                className={`${petFriendlyError && "pl-1 border rounded-lg border-red-500"} my-2 col-start-6 col-end-9`}>
+                                <YNButton
+                                    label={Text.pet}
+                                    name="petFriendly"
+                                    required={true}
+                                    value={petFriendly}
+                                    onChange={(e) => setPetFriendly(e.target.value)}
+                                />
+                            </section>
+                            <section className={"col-start-1 col-end-5"}>
+                                <LabelAsterisk
+                                    label={Text.num_bath}
+                                    className={"label"}
+                                />
+                                <Dropdown
+                                    options={options}
+                                    onChange={handleNumBathChange}
+                                    dropdownCSS={numBathError ? dropdownErrorCSS : dropdownDefaultCSS}
+                                />
+
+                            </section>
+
+
+                            <section
+                                className={`${smokeFriendlyError && "pl-1 border rounded-lg border-red-500"} my-2 col-start-6 col-end-9`}>
+                                <YNButton
+                                    label={Text.smoking}
+                                    name="smokeFriendly"
+                                    required={true}
+                                    value={smokeFriendly}
+                                    onChange={(e) => setSmokeFriendly(e.target.value)}
+                                />
+                            </section>
+                        </div>
+
+                        <LargeTextArea
+                            className={`${fullDescriptionError && "border-red-500"} input`}
+                            rows={"6"}
+                            label={Text.full_des}
+                            labelClassName={"label"}
+                            required={true}
+                            onChange={(e) => setFullDescription(e.target.value)}
+                        />
+
+
+                        <label className="label"> Photos </label>
+                        <Tooltip
+                            text={ToolTipText.PHOTOS}
+                            toolTipID={"UploadPhotos"}
+                        />
+                        <UploadImage handleImageUpload={handleImageUpload}/>
+
+                    </div>
+                </div>
+            </div>
+            <SubmitButton className={"btn btn-green form-btn w-1/2"} onClick={onCreateListing}
+                          onSubmit={onCreateListing}/>
+        </div>
+    )
+}
+
+MemberHomeShareForm.propTypes = {
+    onSubmit: PropTypes.func,
+}
+
+export default MemberHomeShareForm;
