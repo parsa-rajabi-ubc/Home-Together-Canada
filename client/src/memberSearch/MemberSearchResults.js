6c9a2c58fa1e0b0406d67107775673c09b259e44
Parsa <ParsaRajabiPR@gmail.com>
2021-02-08 23:07:50 -0800

    HTC-559: Added Paginate to Member Search Results

--- client/src/memberSearch/MemberSearchResults.js
+++ client/src/memberSearch/MemberSearchResults.js
@@ -12,6 +12,7 @@ import React from 'react';
 import ProfileCard from "./ProfileCard";
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
+import Paginate from "../common/forms/Paginate";
 
 function MemberSearchResults(props) {
     const {profileData} = props;
@@ -36,7 +37,7 @@ function MemberSearchResults(props) {
     }
     return (
         <div>
-            {profiles}
+            <Paginate data={profiles} resultsPerPage={7}/>
         </div>
     );
 }
