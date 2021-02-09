5f8df44689fade9998a7fd514b6469c9ab123564
Parsa <ParsaRajabiPR@gmail.com>
2021-02-09 14:36:49 -0800

    HTC-335: Added Styling to Delete Account

--- client/src/accountSummary/accountDeactivateAndDelete/DeleteAccount.js
+++ client/src/accountSummary/accountDeactivateAndDelete/DeleteAccount.js
@@ -12,20 +12,34 @@ import Button from "../../common/forms/Button";
 
 function DeleteAccount(props) {
 
-    const{confirm, setConfirm, handleDeleteAccount} = props
+    const {confirm, setConfirm, handleDeleteAccount} = props
 
-    return(
+    return (
         <div>
-            <Button label={"Delete Account?"} value={"Delete Account"} onClick={handleDeleteAccount}/>
+            <h3 className="account-summary-info-header">Delete Account</h3>
+            <p className="account-summary-info-text">
+                To delete your account, please confirm using the checkbox below and then select Delete Account.
+            </p>
 
-            <Checkbox
-                label={"Are you sure you'd like to delete your account?"}
-                checked={confirm}
-                onChange={()=>setConfirm(!confirm)}
-            />
+            <div className={"m-auto w-1/2"}>
+                <Checkbox
+                    label={"Are you sure you'd like to delete your account?"}
+                    checked={confirm}
+                    fontNormal={true}
+                    className={"mr-2 my-16"}
+                    onChange={() => setConfirm(!confirm)}
+                />
+            </div>
+
+            {confirm &&
+            <div className={"my-6"}>
+                <Button className={"btn btn-red m-auto w-1/2"} value={"Delete Account"} onClick={handleDeleteAccount}/>
+            </div>}
+
         </div>
     );
 }
+
 DeleteAccount.propTypes = {
     confirm: PropTypes.bool.isRequired,
     setConfirm: PropTypes.func.isRequired,
