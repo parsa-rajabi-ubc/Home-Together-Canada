a7bb7ada753b0751ac7812a1dd6f5f01847f6593
Parsa <ParsaRajabiPR@gmail.com>
2021-02-08 16:55:53 -0800

    HTC-546: Removed side-menu bar and moved search filter to main page

--- client/src/memberSearch/MemberSearchContainer.js
+++ client/src/memberSearch/MemberSearchContainer.js
@@ -6,55 +6,30 @@
  *
  */
 
-import React, { useState } from 'react';
+import React from 'react';
 import {USER_TYPES} from "../common/constants/users";
 import InvalidUser from "./InvalidUser";
 import PropTypes from "prop-types";
 import {compose} from "redux";
 import {withRouter} from "react-router-dom";
 import {connect} from "react-redux";
-import BurgerMenu from "../common/listings/BurgerMenu";
 import SearchResultsContainer from "./SearchResultsContainer";
 import MockProfileCardData from "../mockData/MockProfileCardData";
 import SearchFilterContainer from "./SearchFilterContainer";
 
 const MemberSearchContainer = (props) => {
-    const [menuOpenState, setMenuOpenState] = useState(false)
-
-    const toggleMenu = () => {
-        setMenuOpenState(!menuOpenState);
-    }
-
-    const handleMenuOnClose = () => {
-        setMenuOpenState(false);
-    }
-
     const {accountType, authenticated} = props;
 
     return (
         <div>
             {/* Checking to Ensure User is Authenticated and is a Member to view this page*/}
-            {(!authenticated || accountType !== USER_TYPES.MEMBER) ?
-                <InvalidUser/>
-                :
+            {(!authenticated || accountType !== USER_TYPES.MEMBER) ? <InvalidUser/> :
                 <div className={"flex flex-nowrap"}>
-                    <BurgerMenu isOpen={menuOpenState} onClose={handleMenuOnClose}
-                                content={<SearchFilterContainer toggleSidebar={toggleMenu}/>}
-                    />
+                    <SearchFilterContainer/>
+
                     {/*Results*/}
-                    <div className={"w-1/3 "}>
-                        <button className={"btn btn-green w-1/5 py-2 ml-6 mb-6 text-base my-10"}
-                                onClick={toggleMenu}>Filter
-                        </button>
-                        <SearchResultsContainer profileData={MockProfileCardData}/>
-                    </div>
-
-
-                    {/*Map*/}
                     <div className={"flex-1"}>
-                        <p>
-                            Google Maps Placeholder
-                        </p>
+                        <SearchResultsContainer profileData={MockProfileCardData}/>
                     </div>
                 </div>
             }
