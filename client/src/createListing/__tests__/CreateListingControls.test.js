5c0c4b87b6d0377eeb45bcf0791553a06d6b26a3
Parsa <ParsaRajabiPR@gmail.com>
2021-02-11 18:28:57 -0800

    HTC-436: Updated Tests for CreateListing

--- client/src/createListing/__tests__/CreateListingControls.test.js
+++ client/src/createListing/__tests__/CreateListingControls.test.js
@@ -7,19 +7,32 @@
  */
 
 import React from 'react';
-import renderer from  'react-test-renderer'
+import renderer from 'react-test-renderer'
 import CreateListingControls from "../CreateListingControls";
 
 jest.mock("react-tooltip/node_modules/uuid", () => ({
-            v4: () => "00000000-0000-0000-0000-000000000000"}
+            v4: () => "00000000-0000-0000-0000-000000000000"
+        }
     )
 );
 
 describe('CreateListingControls', () => {
     describe('Container test', () => {
-        it('should match snapshot test', () => {
+        it('should match snapshot test if user is a member', () => {
             // given
             const props = {
+                isUserMember: true
+            };
+            //when
+            const component = renderer.create(<CreateListingControls {...props}/>);
+            const tree = component.toJSON();
+            //then
+            expect(tree).toMatchSnapshot();
+        });
+        it('should match snapshot test if user is a business', () => {
+            // given
+            const props = {
+                isUserMember: false
             };
             //when
             const component = renderer.create(<CreateListingControls {...props}/>);
