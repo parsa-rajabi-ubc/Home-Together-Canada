c973af99f6bd1aa0a64f91e94f88ca44e774ece4
Parsa <ParsaRajabiPR@gmail.com>
2021-02-11 23:24:37 -0800

    HTC-547: Created SearchArea Component

---
+++ client/src/common/listings/SearchArea.js
@@ -0,0 +1,82 @@
+/**
+ * @Author:     Parsa Rajabi
+ * @Created:    2021.2.11
+ *
+ * @Description: Search Area component
+ *
+ */
+
+import React, {useState, useEffect} from 'react';
+import radii from "../forms/Radii";
+import {getProvinces, getCities} from "../utils/locationUtils";
+import Dropdown from "../forms/Dropdown";
+import PropTypes from "prop-types";
+import {dropdownSearchAreaCSS} from "../../css/dropdownCSSUtil"
+
+
+function InterestedArea(props) {
+    const {onChange} = props;
+    const [selectedProvince, setSelectedProvince] = useState();
+    const [selectedCity, setSelectedCity] = useState();
+    const [selectedRadius, setSelectedRadius] = useState();
+
+    const handleProvinceChange = e => {
+        setSelectedProvince(e.value);
+    }
+    const handleCityChange = e => {
+        setSelectedCity(e.value);
+    }
+    const handleRadiusChange = e => {
+        setSelectedRadius(e.value);
+    }
+
+    useEffect(() => {
+        onChange({
+            province: selectedProvince,
+            city: selectedCity,
+            radius: selectedRadius
+        })
+    }, [selectedProvince, selectedCity, selectedRadius])
+
+
+    return (
+        <div>
+            <div className="grid grid-cols-10 gap-x-2">
+                <div className="col-start-1 col-end-4">
+                    <Dropdown isSearchable={true} placeholder={"Province"}
+                              name="province"
+                              options={getProvinces()}
+                              onChange={e => handleProvinceChange(e)}
+                              dropdownCSS={dropdownSearchAreaCSS}
+                    />
+                </div>
+                <div className="col-start-4 col-end-8">
+                    <Dropdown isSearchable={true} placeholder={"City"}
+                              name="city"
+                              options={getCities(selectedProvince)}
+                              onChange={e => handleCityChange(e)}
+                              dropdownCSS={dropdownSearchAreaCSS}
+                    />
+                </div>
+                <div className="col-start-8 col-end-11">
+                    <Dropdown
+                        isSearchable={true}
+                        placeholder={"Radius"}
+                        name="radius"
+                        options={radii}
+                        onChange={e => handleRadiusChange(e)}
+                        dropdownCSS={dropdownSearchAreaCSS}
+                    />
+                </div>
+
+            </div>
+        </div>
+
+    );
+}
+
+InterestedArea.propTypes = {
+    onChange: PropTypes.func,
+}
+
+export default InterestedArea;
