bf9d93045ce4ebbde579aba23dec3767b6fb4faf
Parsa <ParsaRajabiPR@gmail.com>
2021-02-12 21:43:36 -0800

    HTC-583: Updated bullet points, added new business picture

--- client/src/registration/MainLandingPage.js
+++ client/src/registration/MainLandingPage.js
@@ -9,7 +9,42 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import member from "../images/member.svg";
-import business from "../images/business-yellow.svg";
+import business from "../images/business.jpg";
+
+const memberAccountTitle = "Home Share Member Account";
+const businessAccountTitle = "Local Business & Home Sharing Services Account";
+
+const memberAccountText = [
+    "Free account registration and profile creation",
+    "Search and message other members",
+    "Find Home Sharing Businesses and Services",
+    "Member only: Business & Service rating system you can trust"
+];
+
+const businessAccountText = [
+    "Free account registration",
+    "Create free home sharing service listings",
+    "Create paid local classified listings",
+    " Benefit from: Member only rating system you can trust"
+];
+
+const memberAccountBullets = [];
+for (let i = 0; i < memberAccountText.length; i++) {
+    memberAccountBullets.push(
+        <li key={memberAccountText[i]}>
+            {memberAccountText[i]}
+        </li>
+    );
+}
+
+const businessAccountBullets = [];
+for (let i = 0; i < businessAccountText.length; i++) {
+    businessAccountBullets.push(
+        <li key={businessAccountText[i]}>
+            {businessAccountText[i]}
+        </li>
+    );
+}
 
 function MainLandingPage() {
     return (
@@ -21,13 +56,10 @@ function MainLandingPage() {
                          alt="member picture">
                     </img>
                     <div className="m-10 mb-2 text-center text-gray-800">
-                        <p className="mb-2 text-3xl font-bold leading-none text-gray-800">Member Account</p>
+                        <p className="mb-2 text-3xl font-bold leading-none text-gray-800">{memberAccountTitle}</p>
                         <div className="mb-10 text-center">
                             <ul className="inline-block my-3 text-lg text-left list-disc list-inside">
-                                <li>Free account registration and profile creation</li>
-                                <li>Create free Home Sharing listings</li>
-                                <li>Search and message other members</li>
-                                <li>Report any suspicious activity on the site</li>
+                                {memberAccountBullets}
                             </ul>
                         </div>
                     </div>
@@ -35,19 +67,16 @@ function MainLandingPage() {
 
                     <Link to={'/registration/business'} className="flex flex-col w-full pt-2 mx-10 bg-yellow-200 rounded-lg shadow-2xl hover:bg-yellow-400 ">
 
-                    <img className="w-auto h-64 mt-10"
+                    <img className="mt-10"
                          src={business}
                          alt="member picture">
                     </img>
 
                     <div className="m-10 mb-2 text-center text-gray-800">
-                        <p className="mb-2 text-3xl font-bold leading-none text-gray-800">Business & Service Account</p>
+                        <p className="mb-2 text-3xl font-bold leading-none text-gray-800">{businessAccountTitle}</p>
                         <div className="text-center">
                             <ul className="inline-block my-3 text-lg text-left list-disc list-inside">
-                                <li>Free account registration</li>
-                                <li>Create free service listings</li>
-                                <li>Create paid classified listings</li>
-                                <li>Report any suspicious activity on the site</li>
+                                {businessAccountBullets}
                             </ul>
                         </div>
                     </div>
