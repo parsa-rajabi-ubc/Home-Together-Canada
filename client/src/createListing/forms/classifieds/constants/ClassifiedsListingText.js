1b3a4faf40b47d716159f68c4d5438ff618df44b
QinAlex <qinzhenyu1123@gmail.com>
2021-02-13 00:45:31 -0800

    HTC-450: Creating listing for both house and yard

---
+++ client/src/createListing/forms/classifieds/constants/ClassifiedsListingText.js
@@ -0,0 +1,15 @@
+/**
+ * @Author:     Alex Qin
+ * @Created:    2021.02.12
+ *
+ * @Description: Constants for Listing Forms
+ *
+ */
+export const HOUSE_YARD_TEXT = {
+    form_title: "House & Yard Services",
+    title: "Descriptive Title",
+    short_des: "Short Description",
+    full_des: "Full Description",
+    pictures: "Pictures",
+    rateAndFees: "Rates and Fees (CAD)",
+}
