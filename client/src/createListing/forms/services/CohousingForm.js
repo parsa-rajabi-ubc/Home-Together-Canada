71b14105b7bd294ad592683274bbfc852c286a18
Parsa <ParsaRajabiPR@gmail.com>
2021-02-15 12:44:33 -0800

    HTC-453: Updated Services Forms

--- client/src/createListing/forms/services/CohousingForm.js
+++ client/src/createListing/forms/services/CohousingForm.js
@@ -84,7 +84,6 @@ const CohousingForm = (props) => {
             onSubmit();
         }
     }
-
     return (
         <div>
             <div className="col-start-1 col-end-7 py-5 px-5 m-6 bg-white shadow-lg rounded-xl">
