a255027d0a6cc20101bd3f3658a43178d9a53391
Lightbrite88 <lightbrite88@gmail.com>
2021-02-16 17:35:54 -0700

    HTC-567: Listing mock data for members and businesses, content included

---
+++ client/src/mockData/MockListingData.js
@@ -0,0 +1,238 @@
+/**
+ * @Author:     Jeff Hatton
+ * @Created:    2020.02.16
+ *
+ * @Description: Const array of mock listings - includes listings for members and businesses
+ *
+ */
+
+export const mockMemberListings = [
+    {
+        title: "Room for Rent",
+        shortDescription: "Furnished Room, Utilities Covered",
+        monthlyCost: 750,
+        petFriendly: false,
+        smokeFriendly: false,
+        datePosted: "2021-02-14T02:42:39.005Z"
+    },
+    {
+        title: "Room for Rent",
+        shortDescription: "Unfurnished Room, Utilities/internet Split",
+        monthlyCost: 500,
+        petFriendly: true,
+        smokeFriendly: true,
+        datePosted: "2021-02-15T05:42:39.000Z"
+    },
+    {
+        title: "Basement suite",
+        shortDescription: "1 bed 1 bath, nice part of town, parking spot",
+        monthlyCost: 1100,
+        petFriendly: false,
+        smokeFriendly: false,
+        datePosted: "2021-02-14T16:42:39.000Z"
+    },
+    {
+        title: "Room for Rent",
+        shortDescription: "Furnished Room, Utilities Covered",
+        monthlyCost: 850,
+        petFriendly: false,
+        smokeFriendly: true,
+        datePosted: "2021-02-16T23:42:39.000Z"
+    },
+    {
+        title: "Room for Rent",
+        shortDescription: "Furnished Room, Utilities Covered, Private Kitchenette",
+        monthlyCost: 950,
+        petFriendly: true,
+        smokeFriendly: true,
+        datePosted: "2021-02-16T03:42:39.000Z"
+    },
+    {
+        title: "Room for Rent",
+        shortDescription: "Unfurnished Room, spacious living area ",
+        monthlyCost: 775,
+        petFriendly: false,
+        smokeFriendly: false,
+        datePosted: "2021-02-15T11:42:39.000Z"
+    },
+    {
+        title: "Room for Rent",
+        shortDescription: "Unfurnished Room, large kitchen storage",
+        monthlyCost: 750,
+        petFriendly: true,
+        smokeFriendly: false,
+        datePosted: "2021-02-13T18:42:39.000Z"
+    },
+    {
+        title: "Room for Rent",
+        shortDescription: "Furnished Room, significant storage space",
+        monthlyCost: 650,
+        petFriendly: false,
+        smokeFriendly: false,
+        datePosted: "2021-02-13T05:42:39.000Z"
+    },
+    {
+        title: "Room for Rent",
+        shortDescription: "Furnished Room, private bathroom",
+        monthlyCost: 750,
+        petFriendly: true,
+        smokeFriendly: false,
+        datePosted: "2021-02-15T05:42:39.000Z"
+    },
+    {
+        title: "Room for Rent",
+        shortDescription: "Furnished Room, fast internet",
+        monthlyCost: 850,
+        petFriendly: false,
+        smokeFriendly: false,
+        datePosted: "2021-02-16T01:42:39.000Z"
+    },
+    {
+        title: "Room for Rent",
+        shortDescription: "Furnished, private bath",
+        monthlyCost: 950,
+        petFriendly: true,
+        smokeFriendly: false,
+        datePosted: "2021-02-16T02:42:39.000Z"
+    },
+    {
+        title: "Room for Rent",
+        shortDescription: "Furnished Room, Utilities Covered",
+        monthlyCost: 750,
+        petFriendly: true,
+        smokeFriendly: true,
+        datePosted: "2021-02-14T08:42:39.000Z"
+    },
+    {
+        title: "Rooms for Rent",
+        shortDescription: "3 Furnished Rooms, for students preferably",
+        monthlyCost: 650,
+        petFriendly: false,
+        smokeFriendly: true,
+        datePosted: "2021-02-14T07:42:39.000Z"
+    },
+    {
+        title: "Room for Rent",
+        shortDescription: "Furnished Room, beautiful house",
+        monthlyCost: 800,
+        petFriendly: true,
+        smokeFriendly: false,
+        datePosted: "2021-02-15T05:42:39.000Z"
+    },
+    {
+        title: "Basement suite for Couple",
+        shortDescription: "Furnished Large Room, Utilities Covered",
+        monthlyCost: 1300,
+        petFriendly: true,
+        smokeFriendly: true,
+        datePosted: "2021-02-13T12:42:39.000Z"
+    }
+];
+
+export const mockBusinessListings = [
+    {
+        logo: "public/uploads/logo1",
+        title: "Apartments for 2 available",
+        businessName: "Kelowna Apartment Inc.",
+        shortDescription: "2 bed 2 bath - furnished",
+        datePosted: "2021-02-14T22:42:39.000Z"
+    },
+    {
+        logo: "public/uploads/logo2",
+        title: "Apartments for 3 available",
+        businessName: "Kelowna Apartment Inc.",
+        shortDescription: "3 bed 2 bath - furnished",
+        datePosted: "2021-02-15T05:42:39.000Z"
+    },
+    {
+        logo: "public/uploads/logo3",
+        title: "House with 4 rooms available",
+        businessName: "Kelowna House Rentals",
+        shortDescription: "4 bed 3 bath - furnished",
+        datePosted: "2021-02-14T16:42:39.000Z"
+    },
+    {
+        logo: "public/uploads/logo4",
+        title: "Beautiful House On Lakefront For Rent",
+        businessName: "Lakefront Properties",
+        shortDescription: "2 bed 2 bath - furnished",
+        datePosted: "2021-02-16T23:42:39.000Z"
+    },
+    {
+        logo: "public/uploads/logo5",
+        title: "Vacuum Rentals",
+        businessName: "Kelowna Apartment Inc.",
+        shortDescription: "2 bed 2 bath - furnished",
+        datePosted: "2021-02-16T03:42:39.000Z"
+    },
+    {
+        logo: "public/uploads/logo6",
+        title: "Lawn Management Services",
+        businessName: "Lawns-R-Us",
+        shortDescription: "2 bed 2 bath - furnished",
+        datePosted: "2021-02-15T11:42:39.000Z"
+    },
+    {
+        logo: "public/uploads/logo7",
+        title: "Carpet Cleaning Rentals",
+        businessName: "Carpet Cleaners",
+        shortDescription: "We offer services, or tool rentals",
+        datePosted: "2021-02-13T18:42:39.000Z"
+    },
+    {
+        logo: "public/uploads/logo8",
+        title: "Plumbing Services Available",
+        businessName: "Kelowna Plumbing Inc.",
+        shortDescription: "2 bed 2 bath - furnished",
+        datePosted: "2021-02-13T05:42:39.000Z"
+    },
+    {
+        logo: "public/uploads/logo9",
+        title: "Apartment Rooms Available",
+        businessName: "Kelowna Apartment Inc.",
+        shortDescription: "Apartments of various sizes",
+        datePosted: "2021-02-15T05:42:39.000Z"
+    },
+    {
+        logo: "public/uploads/logo10",
+        title: "Window Cleaning Services",
+        businessName: "Kelowna Glass Cleaners",
+        shortDescription: "We clean residential or high rise complexes",
+        datePosted: "2021-02-16T01:42:39.000Z"
+    },
+    {
+        logo: "public/uploads/logo11",
+        title: "Apartments for 2 available",
+        businessName: "Kelowna Apartment Inc.",
+        shortDescription: "2 bed 1 bath - unfurnished",
+        datePosted: "2021-02-16T02:42:39.000Z"
+    },
+    {
+        logo: "public/uploads/logo12",
+        title: "Apartments for 2 available",
+        businessName: "Kelowna Apartment Inc.",
+        shortDescription: "2 bed 2 bath - Unfurnished",
+        datePosted: "2021-02-14T08:42:39.000Z"
+    },
+    {
+        logo: "public/uploads/logo13",
+        title: "Apartments for 3 available",
+        businessName: "Kelowna Apartment Inc.",
+        shortDescription: "3 bed 2 bath - Unfurnished",
+        datePosted: "2021-02-14T07:42:39.000Z"
+    },
+    {
+        logo: "public/uploads/logo14",
+        title: "Senior services",
+        businessName: "Senior services",
+        shortDescription: "Mobility tools, awareness of area services, financial aid",
+        datePosted: "2021-02-15T05:42:39.000Z"
+    },
+    {
+        logo: "public/uploads/logo15",
+        title: "Shared Vehicle Insurance",
+        businessName: "Kelowna Vehicle Insurance",
+        shortDescription: "Insurance and help available for groups sharing cars",
+        datePosted: "2021-02-13T12:42:39.000Z"
+    }
+];
