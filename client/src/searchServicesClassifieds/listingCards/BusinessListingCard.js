3b61cedadeed4947ad9c0daa154ba5cbe1061a34
Parsa <ParsaRajabiPR@gmail.com>
2021-02-19 02:39:59 -0800

    HTC-565: Added Content + CSS to BusinessListingCard

--- client/src/searchServicesClassifieds/listingCards/BusinessListingCard.js
+++ client/src/searchServicesClassifieds/listingCards/BusinessListingCard.js
@@ -9,15 +9,31 @@
 
 import React from 'react';
 import PropTypes from "prop-types";
+import Moment from 'react-moment';
+import HTC_Logo from "../../images/HTC_Logo.jpg";
 
 
 function BusinessListingCard(props) {
     const {logo, title, businessName, shortDescription, datePosted} = props;
 
     return (
-        <div>
+        <section className={"card-container mt-0"}>
+            {/*TODO: replace HTC_Logo with logo Prop*/}
+            <img className={"float-left w-24 h-auto mx-4 "} src={HTC_Logo} alt={""}/>
+            <div className={"inline align-middle "}>
+                <label className={"font-semibold justify-between"}> {title} </label>
+                {/*{shortDescription}*/}
+                <section className={"inline float-right pr-6 justify-end"}>
+                    <Moment format="MMM D, YYYY">{datePosted}</Moment>
+                </section>
+            </div>
 
-        </div>
+            <div className={"mx-6 inline items-center justify-between leading-tight font-light"}>
+                <section className={"mb-3"}> {businessName} </section>
+                <section> {shortDescription} </section>
+
+            </div>
+        </section>
     );
 }
 
