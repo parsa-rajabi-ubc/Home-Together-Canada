55070e643761b95b3560484d77291443889ada75
Parsa <ParsaRajabiPR@gmail.com>
2021-02-18 17:20:34 -0800

    HTC-568: Created Search Listing Filters Container as Child with React Context

---
+++ client/src/searchServicesClassifieds/SearchListingFiltersContainer.js
@@ -0,0 +1,24 @@
+/**
+ * @Author:     Parsa Rajabi
+ * @Created:    2021.2.18
+ *
+ * @Description: Search Listing Filter Container
+ *
+ */
+
+import React, {useContext} from 'react';
+import {listingContext} from "./SearchListingContainer";
+
+function SearchListingFiltersContainer() {
+
+    const listingPage = useContext(listingContext);
+
+    return (
+            <div>
+                {listingPage}
+            </div>
+    );
+}
+
+
+export default SearchListingFiltersContainer;
