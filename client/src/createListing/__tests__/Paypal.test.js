062fa6bab7fb1d9217622eecebcb87599edabe2f
Parsa <ParsaRajabiPR@gmail.com>
2021-02-19 17:38:10 -0800

    HTC-591: Updated test and snapshot

--- client/src/createListing/__tests__/Paypal.test.js
+++ client/src/createListing/__tests__/Paypal.test.js
@@ -8,36 +8,14 @@
 
 import React from 'react';
 import renderer from 'react-test-renderer'
-import Paypal, {PAYMENT_STATUS} from "../Paypal";
+import Paypal from "../Paypal";
 
 describe('Paypal', () => {
     describe('Snapshot test', () => {
         it('should match snapshot test regardless of properties', () => {
             // given
             const props = {
-                paymentStatus: PAYMENT_STATUS.APPROVED
-            };
-            //when
-            const component = renderer.create(<Paypal {...props}/>);
-            const tree = component.toJSON();
-            //then
-            expect(tree).toMatchSnapshot();
-        });
-        it('should match snapshot test regardless of properties', () => {
-            // given
-            const props = {
-                paymentStatus: PAYMENT_STATUS.CANCELLED
-            };
-            //when
-            const component = renderer.create(<Paypal {...props}/>);
-            const tree = component.toJSON();
-            //then
-            expect(tree).toMatchSnapshot();
-        });
-        it('should match snapshot test regardless of properties', () => {
-            // given
-            const props = {
-                paymentStatus: PAYMENT_STATUS.ERROR
+                paymentStatus: jest.fn()
             };
             //when
             const component = renderer.create(<Paypal {...props}/>);
