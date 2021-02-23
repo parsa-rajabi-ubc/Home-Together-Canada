1012fd60d6f52817b0a61976affc23d61bae5830
Parsa <ParsaRajabiPR@gmail.com>
2021-02-23 11:23:21 -0800

    HTC-630: Added Props to ListingResults.js

--- client/src/searchServicesClassifieds/listingResults/ListingResults.js
+++ client/src/searchServicesClassifieds/listingResults/ListingResults.js
@@ -8,17 +8,22 @@
  */
 
 import React, {useContext, useEffect, useState} from 'react';
+import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 import Paginate from "../../common/forms/Paginate";
 import {listingContext, PAGE_NAMES} from "../SearchListingContainer";
 import BusinessListingCard from "../listingCards/BusinessListingCard";
-import {mockBusinessListings, mockMemberListings} from "../../mockData/MockListingData";
 import MemberListingCard from "../listingCards/MemberListingCard";
+import {USER_TYPES as USER_TYPE} from "../../common/constants/users";
+import HTC_Logo from "../../images/HTC_Logo.jpg"
 
 const NUM_RESULTS = 7;
 
-function ListingResults() {
+function ListingResults(props) {
+    const {listingData, listingUser} = props;
 
+    const [cardData, setCardData] = useState([]);
+    const [loading, setLoading] = useState(true);
     const listingPage = useContext(listingContext);
 
     const businessCards = mockBusinessListings.map(
@@ -59,6 +64,11 @@ function ListingResults() {
         </div>
     );
 }
+
+ListingResults.propTypes = {
+    listingData: PropTypes.array.isRequired,
+    listingUser: PropTypes.string.isRequired,
+};
 
 
 export default ListingResults;
