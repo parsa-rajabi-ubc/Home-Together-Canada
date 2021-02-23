e7e5312105710b59b39321c7d44f5b1e711f951c
Parsa <ParsaRajabiPR@gmail.com>
2021-02-23 11:24:09 -0800

    HTC-630: Added useEffect, Added listing to state variable, Added Loading Condition

--- client/src/searchServicesClassifieds/listingResults/ListingResults.js
+++ client/src/searchServicesClassifieds/listingResults/ListingResults.js
@@ -11,7 +11,7 @@ import React, {useContext, useEffect, useState} from 'react';
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 import Paginate from "../../common/forms/Paginate";
-import {listingContext, PAGE_NAMES} from "../SearchListingContainer";
+import {listingContext} from "../SearchListingContainer";
 import BusinessListingCard from "../listingCards/BusinessListingCard";
 import MemberListingCard from "../listingCards/MemberListingCard";
 import {USER_TYPES as USER_TYPE} from "../../common/constants/users";
@@ -26,41 +26,57 @@ function ListingResults(props) {
     const [loading, setLoading] = useState(true);
     const listingPage = useContext(listingContext);
 
-    const businessCards = mockBusinessListings.map(
-        (business) =>
-            <Link to={`/${listingPage}/${business.id}`} key={business.id}>
-                <BusinessListingCard
-                    logo={business.logo}
-                    title={business.title}
-                    businessName={business.businessName}
-                    shortDescription={business.shortDescription}
-                    datePosted={business.datePosted}/>
-            </Link>
-    )
-
-    const memberListingCards = mockMemberListings.map(
-        (member) =>
-            <Link to={`/${listingPage}/${member.id}`} key={member.id}>
-                <MemberListingCard
-                    title={member.title}
-                    monthlyCost={member.monthlyCost}
-                    petFriendly={member.petFriendly}
-                    smokeFriendly={member.smokeFriendly}
-                    shortDescription={member.shortDescription}
-                    datePosted={member.datePosted}/>
-            </Link>
-    )
-
-    const [listingCards, setListingCards] = useState((listingPage === PAGE_NAMES.SERVICES) ? businessCards.concat(memberListingCards) : businessCards)
-
     useEffect(() => {
-        setListingCards((listingPage === PAGE_NAMES.SERVICES) ? businessCards.concat(memberListingCards) : businessCards)
-    }, [listingPage]);
+        if (listingUser === USER_TYPE.MEMBER) {
+            setCardData(listingData.map(
+                (listing) =>
+                    <Link
+                        to={{
+                            pathname: `/${listingPage}/${listing.id}`,
+                            state: {
+                                listing: listing
+                            }
+                        }}
+                        key={listing.id}
+                    >
+                        <MemberListingCard
+                            title={listing.title}
+                            monthlyCost={listing.monthlyCost}
+                            petFriendly={listing.petFriendly}
+                            smokeFriendly={listing.smokeFriendly}
+                            shortDescription={listing.shortDescription}
+                            datePosted={listing.createdAt}/>
+                    </Link>
+            ));
+            setLoading(false);
+        } else {
+            setCardData(listingData.map(
+                (listing) =>
+                    <Link
+                        to={{
+                            pathname: `/${listingPage}/${listing.id}`,
+                            state: {
+                                listing: listing
+                            }
+                        }}
+                        key={listing.id}
+                    >
+                        <BusinessListingCard
+                            logo={HTC_Logo}
+                            title={listing.title}
+                            businessName={listing.business.businessName}
+                            shortDescription={listing.shortDescription}
+                            datePosted={listing.createdAt}/>
+                    </Link>
+            ));
+            setLoading(false);
+        }
+    }, []);
 
 
     return (
         <div>
-            <Paginate data={listingCards} resultsPerPage={NUM_RESULTS}/>
+            {!loading && <Paginate data={cardData} resultsPerPage={NUM_RESULTS}/>}
         </div>
     );
 }
