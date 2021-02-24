c9676a8dfdadbab352c2f3e0da39bd08c195882b
Parsa <ParsaRajabiPR@gmail.com>
2021-02-23 17:26:31 -0800

    HTC-630: Added condition for when user tries to access listing through URL, Updated condition to set businessInfo based on category

--- client/src/searchServicesClassifieds/BusinessListingContainer.js
+++ client/src/searchServicesClassifieds/BusinessListingContainer.js
@@ -24,9 +24,21 @@ import RentalsCustomFields from "./listings/customFields/classifieds/RentalsCust
 import AgenciesCustomFields from "./listings/customFields/classifieds/AgenciesCustomFields";
 import EventsCustomFields from "./listings/customFields/classifieds/EventsCustomFields";
 import HouseServicesCustomFields from "./listings/customFields/classifieds/HouseServicesCustomFields";
+import Confirmation from "../common/listings/Confirmation";
+import {useHistory} from "react-router-dom";
+import {PAGE_NAMES} from "./SearchListingContainer";
+
+const MESSAGE = {
+    INVALID_PAGE: "Please click on listing cards on search page to view listings",
+    INVALID_PAGE_BUTTON_TEXT: "Back to Search Page"
+}
 
 function BusinessListingContainer(props) {
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+
+    const listingPage = useHistory().location.pathname;
+    const [redirectTo, setRedirectTo] = useState('/');
 
     // Common Fields
     const [title, setTitle] = useState();
@@ -72,10 +84,28 @@ function BusinessListingContainer(props) {
     useEffect(() => {
         setLoading(true);
 
-        const {listing} = props.location.state;
+        // set listing if redux store exists
+        if (props.location.state) {
+            const {listing} = props.location.state;
 
-        setCommonListingData(listing);
-        setCustomFieldData(listing);
+            setCommonListingData(listing);
+            setCustomFieldData(listing);
+
+            // only set business info if the category name is not member
+            if (listing.categoryName !== MEMBER_SERVICE_CATEGORIES.MEMBER_HOME) {
+                setBusinessInfo(listing)
+            }
+            // display error if user tries to access page through URL
+            // redirect user to right searching page based on their URL
+        } else {
+            setError(true);
+            if (listingPage.includes(PAGE_NAMES.SERVICES)) {
+                setRedirectTo("/" + PAGE_NAMES.SERVICES)
+            } else if (listingPage.includes(PAGE_NAMES.CLASSIFIEDS)) {
+                setRedirectTo("/" + PAGE_NAMES.CLASSIFIEDS)
+            } else
+                setRedirectTo("/");
+        }
     }, []);
 
 
@@ -110,8 +140,8 @@ function BusinessListingContainer(props) {
             case MEMBER_SERVICE_CATEGORIES.MEMBER_HOME:
                 setGeneralLocation(listing.postalCode);
                 setHomeShareMonthlyCost(listing.monthlyCost);
-                setNumBed(listing.numBed.toString());
-                setNumBath(listing.numBath.toString());
+                setNumBed(listing.numBed);
+                setNumBath(listing.numBath);
                 setFurnished(listing.furnished);
                 setPetFriendly(listing.petFriendly);
                 setSmokeFriendly(listing.smokeFriendly);
@@ -121,45 +151,37 @@ function BusinessListingContainer(props) {
                 setContactName(listing.contactName);
                 setUnitForSale(listing.unitsForSale);
                 setUnitForRent(listing.unitsForRent);
-                setBusinessInfo(listing);
                 break;
             case BUSINESS_SERVICE_CATEGORIES.SHARED_HOME_SERVICES:
                 setRateAndFees(listing.rateAndFees);
-                setBusinessInfo(listing);
                 break;
             case BUSINESS_SERVICE_CATEGORIES.SHARED_BUSINESS_SERVICES:
                 setRateAndFees(listing.rateAndFees);
-                setBusinessInfo(listing);
                 break;
             case BUSINESS_SERVICE_CATEGORIES.GOVERNMENT_SERVICES:
                 setContactName(listing.contactName);
                 setContactPhoneNumber(listing.contactPhoneNumber.toString());
-                setBusinessInfo(listing);
                 break;
             case BUSINESS_CLASSIFIEDS_CATEGORIES.RENTALS:
                 setPrice(listing.monthlyCost)
                 setRateAndFees(listing.rateAndFees);
-                setNumBed(listing.numBed.toString());
-                setNumBath(listing.numBath.toString());
+                setNumBed(listing.numBed);
+                setNumBath(listing.numBath);
                 setFurnished(listing.furnished);
                 setPetFriendly(listing.petFriendly);
                 setSmokeFriendly(listing.smokeFriendly);
-                setBusinessInfo(listing);
                 break;
             case BUSINESS_CLASSIFIEDS_CATEGORIES.HOUSE_YARD:
                 setRateAndFees(listing.rateAndFees);
-                setBusinessInfo(listing);
                 break;
             case BUSINESS_CLASSIFIEDS_CATEGORIES.LEGAL_SALES:
                 setRateAndFees(listing.rateAndFees);
-                setBusinessInfo(listing);
                 break;
             case BUSINESS_CLASSIFIEDS_CATEGORIES.CLASSES_CLUBS:
                 setContactName(listing.contactName);
                 setContactPhoneNumber(listing.contactPhoneNumber.toString());
                 setRateAndFees(listing.rateAndFees);
                 setEventDateTime(listing.eventDateTime);
-                setBusinessInfo(listing);
                 break;
         }
         setLoading(false);
@@ -204,49 +226,52 @@ function BusinessListingContainer(props) {
 
     return (
         <div>
-            {loading ?
-                <Loading isLoading={loading}/>
-                :
-                <div className="selected-component-grid-outer">
-                    <div className="selected-component-grid-inner">
-                        <div className={"flex mx-auto my-10 w-full"}>
+            {error ?
+                <Confirmation message={MESSAGE.INVALID_PAGE} redirectTo={redirectTo}
+                              buttonText={MESSAGE.INVALID_PAGE_BUTTON_TEXT}/>
+                : loading ?
+                    <Loading isLoading={loading}/>
+                    :
+                    <div className="selected-component-grid-outer">
+                        <div className="selected-component-grid-inner">
+                            <div className={"flex mx-auto my-10 w-full"}>
 
-                            <section className={"flex-col w-full pr-10"}>
-                                <section className={"flex-none w-full"}>
-                                    <h1 className={"page-title inline"}> {title} </h1>
-                                    {listingCategory === MEMBER_SERVICE_CATEGORIES.MEMBER_HOME &&
-                                    <button
-                                        className={"btn btn-green inline float-right mb-6 w-1/4 px-0 text-base py-2"}>Send
-                                        Message
-                                    </button>
-                                    }
-                                    <p className={"label-result"}> Description </p>
-                                    <p className={"my-2"}> {fullDescription} </p>
+                                <section className={"flex-col w-full pr-10"}>
+                                    <section className={"flex-none w-full"}>
+                                        <h1 className={"page-title inline"}> {title} </h1>
+                                        {listingCategory === MEMBER_SERVICE_CATEGORIES.MEMBER_HOME &&
+                                        <button
+                                            className={"btn btn-green inline float-right mb-6 w-1/4 px-0 text-base py-2"}>Send
+                                            Message
+                                        </button>
+                                        }
+                                        <p className={"label-result"}> Description </p>
+                                        <p className={"my-2"}> {fullDescription} </p>
+                                    </section>
+
+                                    <section className={""}>
+                                        {returnCustomFieldComponent(listingCategory)}
+                                    </section>
                                 </section>
 
-                                <section className={""}>
-                                    {returnCustomFieldComponent(listingCategory)}
+                                {/* Conditionally show Business Info -- hidden  if category is MemberHomeToShare */}
+                                {listingCategory !== MEMBER_SERVICE_CATEGORIES.MEMBER_HOME &&
+                                <section className={"w-1/3"}>
+                                    <BusinessInfo
+                                        logo={logo}
+                                        businessName={businessName}
+                                        address={address}
+                                        isNationWide={isNationWide}
+                                        website={website}
+                                        phone={phone}
+                                        email={email}
+                                    />
                                 </section>
-                            </section>
+                                }
 
-                            {/* Conditionally show Business Info -- hidden  if category is MemberHomeToShare */}
-                            {listingCategory !== MEMBER_SERVICE_CATEGORIES.MEMBER_HOME &&
-                            <section className={"w-1/3"}>
-                                <BusinessInfo
-                                    logo={logo}
-                                    businessName={businessName}
-                                    address={address}
-                                    isNationWide={isNationWide}
-                                    website={website}
-                                    phone={phone}
-                                    email={email}
-                                />
-                            </section>
-                            }
-
+                            </div>
                         </div>
                     </div>
-                </div>
             }
         </div>
 
