948fb1a273484001a8d2da38684018447e830740
Rachelle Gelden <rachelle@gelden.com>
2021-03-10 19:07:29 -0800

    HTC-735: route to give admin privileges

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -9,14 +9,38 @@
 const express = require('express');
 const router = express.Router();
 
-const { isLoggedIn, userIsMember } = require('./routeUtils');
+const { isLoggedIn, userIsMember, userIsAdmin } = require('./routeUtils');
 const memberAccounts = require('../controllers/memberAccountController');
 
+// NOTE: this route is only for development purposes as a means to make the first admin
 router.get('/dev/create/',
     isLoggedIn,
     userIsMember,
     function (req, res, next) {
         memberAccounts.giveAdminPrivileges(req.user.uid)
+            .then(data => {
+                if (data.length) {
+                    res.status(200).json({ adminPrivileges: true });
+                } else {
+                    res.status(500).json({ err: `Failed to give user with id ${req.user.uid} admin privileges`});
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ err });
+            });
+    }
+);
+
+// give another member admin privileges
+router.post('/create/',
+    isLoggedIn,
+    userIsAdmin,
+    async function (req, res, next) {
+        // get the uid for the given username
+        const member = await memberAccounts.findMemberAccountByUsername(req.body.username);
+
+        // give admin privileges
+        memberAccounts.giveAdminPrivileges(member.uid)
             .then(data => {
                 if (data.length) {
                     res.status(200).json({ adminPrivileges: true });
