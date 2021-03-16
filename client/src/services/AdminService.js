525569888b4b73a3c7e52480469c09721ea96bee
Parsa <ParsaRajabiPR@gmail.com>
2021-03-16 10:38:53 -0700

    HTC-762: Added Banned Routes to Admin Service

--- client/src/services/AdminService.js
+++ client/src/services/AdminService.js
@@ -31,3 +31,25 @@ export const promoteMemberToAdmin = (memberUsername) => {
     }
     return fetch(`${DEV_URL}/admin/create/`, request);
 }
+
+export const getAllBannedUsers = () => {
+    return fetch(`${DEV_URL}/admin/banned/users/`, {
+        method: 'GET',
+        withCredentials: true,
+        credentials: 'include'
+    });
+}
+
+export const banUsername = (username) => {
+    const request = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        credentials: 'include',
+        withCredentials: true,
+        body: JSON.stringify(username)
+    }
+    return fetch(`${DEV_URL}/admin/ban/user/`, request);
+}
