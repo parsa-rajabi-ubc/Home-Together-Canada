e9bd1ea3321505a9b1eb7cdeaa9ac8a14bb1d970
Rachelle Gelden <rachelle@gelden.com>
2021-04-04 13:22:26 -0700

    HTC-793: display business logos on listing cards

--- client/src/searchServicesClassifieds/listingCards/BusinessListingCard.js
+++ client/src/searchServicesClassifieds/listingCards/BusinessListingCard.js
@@ -10,7 +10,6 @@
 import React from 'react';
 import PropTypes from "prop-types";
 import Moment from 'react-moment';
-import HTC_Logo from "../../images/HTC_Logo.jpg";
 
 
 function BusinessListingCard(props) {
@@ -18,8 +17,7 @@ function BusinessListingCard(props) {
 
     return (
         <section className={"card-container mt-0"}>
-            {/*TODO: replace HTC_Logo with logo Prop*/}
-            <img className={"float-left w-24 h-auto mx-4 "} src={HTC_Logo} alt={""}/>
+            <img className={"float-left w-24 h-24 mx-4 "} src={logo}/>
             <div className={"inline align-middle "}>
                 <label className={"font-semibold justify-between"}> {title} </label>
                 {/*{shortDescription}*/}
