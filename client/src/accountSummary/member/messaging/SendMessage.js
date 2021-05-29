a01bbc19d3d84a0859bf6cc9daf2f95d805dea89
Rachelle Gelden <rachelle@gelden.com>
2021-05-29 17:10:24 -0600

    HTC-819: member account summary components with field length checks

--- client/src/accountSummary/member/messaging/SendMessage.js
+++ client/src/accountSummary/member/messaging/SendMessage.js
@@ -13,6 +13,7 @@ import LargeTextArea from "../../../common/forms/LargeTextArea";
 import SubmitButton from "../../../common/forms/SubmitButton";
 import {useEffect} from "react";
 import {checkIfErrorsExistInMapping, validateInput} from "../../../registration/registrationUtils";
+import {MESSAGE_FIELD_LENGTHS} from "../../../common/constants/fieldLengths";
 
 function SendMessage(props) {
     //username currently unused, but intended as component which will handle post request to add message to database
@@ -59,13 +60,16 @@ function SendMessage(props) {
     return(
         <div>
             <br/><p>To {receiverUsername}: </p>
-            <LargeTextArea className={`${contentError && "border-red-500"} input`}
-                           label={"What would you like to contact this member about? "}
-                           onChange={(e) => setContent(e.target.value)}
+            <LargeTextArea
+                className={`${contentError && "border-red-500"} input`}
+                label={"What would you like to contact this member about? "}
+                onChange={(e) => setContent(e.target.value)}
+                charLimit={MESSAGE_FIELD_LENGTHS.CONTENT}
             />
-            <SubmitButton   className={"btn btn-green mb-6 w-1/2 text-base py-2"}
-                            onClick={onSubmitMessage}
-                            onSubmit={onSubmitMessage}
+            <SubmitButton
+                className={"btn btn-green mb-6 w-1/2 text-base py-2"}
+                onClick={onSubmitMessage}
+                onSubmit={onSubmitMessage}
             />
         </div>
     )
