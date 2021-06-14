3d118c2cedc181fa4035352bee973436d2da1e5a
Parsa <ParsaRajabiPR@gmail.com>
2021-06-13 18:07:00 -0700

    HTC-846: Created new ModelCSSUtil file for ModelCSS

---
+++ client/src/css/ModelCSSUtil.js
@@ -0,0 +1,13 @@
+export const customModalStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        padding: '60px',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        backgroundColor: 'white',
+        borderRadius: '10px',
+    }
+};
