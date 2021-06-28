f93316f184a2905ec4c47b1fa5cdd357fe5863d1
Parsa <ParsaRajabiPR@gmail.com>
2021-06-27 23:59:19 -0700

    HTC-873: Added support to ManageListingContainer.js for business accounts, removed confirmation, added loading screen

--- client/src/accountSummary/manageListing/ManageListingContainer.js
+++ client/src/accountSummary/manageListing/ManageListingContainer.js
@@ -7,33 +7,28 @@
  */
 
 import React, {useState, useEffect} from "react";
+import PropTypes from "prop-types";
 import * as MemberServices from "../../services/MemberService";
 import * as BusinessServices from "../../services/BusinessService";
 import * as ListingServices from "../../services/ListingService";
-import Confirmation from "../../common/listings/Confirmation";
 import ManageListingResults from "./ManageListingResults";
-import ManageListingTabs, {TAB_LABELS} from "./ManageListingTabs";
+import ManageListingTabs, {BUSINESS_TAB_LABELS, MEMBER_TAB_LABELS} from "./ManageListingTabs";
 import {MANAGE_LISTING_TOAST} from "../../common/constants/ToastText";
 import {toast} from "react-toastify";
+import {withRouter} from "react-router-dom";
+import {USER_TYPES} from "../../common/constants/users";
+import {compose} from "redux";
+import {connect} from "react-redux";
+import Loading from "../../common/loading/Loading";
 
-function ManageListingContainer() {
+function ManageListingContainer(props) {
+    const {accountType} = props;
 
-    const [liveMemberListings, setLiveMemberListings] = useState();
-    const [inactiveMemberListings, setInactiveMemberListings] = useState();
-
-    const [liveBusinessListings, setLiveBusinessListings] = useState();
-    const [inactiveBusinessListings, setInactiveBusinessListings] = useState();
-    const [rejectedBusinessListings, setRejectedBusinessListings] = useState();
-
-    const [selectedTab, setSelectedTab] = useState();
+    const [selectedTab, setSelectedTab] = useState(MEMBER_TAB_LABELS.LIVE);
     const [viewableListingData, setViewableListingData] = useState();
     const [viewableListingTitle, setViewableListingTitle] = useState();
 
-    // useEffects
-    useEffect(() => {
-        getLiveMemberListings();
-        getInactiveMemberListings();
-    }, [])
+    const [loading, setLoading] = useState(true);
 
     // useEffects
     useEffect(() => {
@@ -42,14 +37,32 @@ function ManageListingContainer() {
 
 
     function updateListingData() {
-        switch (selectedTab) {
-            case TAB_LABELS.LIVE:
-                setViewableListingData(liveMemberListings);
-                break;
-            case TAB_LABELS.INACTIVE:
-                setViewableListingData(inactiveMemberListings);
-                break;
+        if (accountType === USER_TYPES.MEMBER) {
+            switch (selectedTab) {
+                case MEMBER_TAB_LABELS.LIVE:
+                    getLiveMemberListings();
+                    break;
+                case MEMBER_TAB_LABELS.INACTIVE:
+                    getInactiveMemberListings();
+                    break;
+            }
+        } else if (accountType === USER_TYPES.BUSINESS) {
+            switch (selectedTab) {
+                case BUSINESS_TAB_LABELS.LIVE:
+                    getLiveBusinessListings()
+                    break;
+                case BUSINESS_TAB_LABELS.INACTIVE:
+                    getInactiveBusinessListings();
+                    break;
+                case BUSINESS_TAB_LABELS.REJECTED:
+                    getRejectedBusinessListings();
+                    break;
+                case BUSINESS_TAB_LABELS.PENDING:
+                    getPendingBusinessListings();
+                    break;
+            }
         }
+        setLoading(false);
     }
 
     function getLiveMemberListings() {
@@ -57,7 +70,7 @@ function ManageListingContainer() {
             .then(res => res.json())
             .then(data => {
                 if (data) {
-                    setLiveMemberListings(data.liveListings);
+                    setViewableListingData(data.liveListings);
                 } else if (data.err) {
                     toast.error(data.err);
                 }
@@ -71,7 +84,7 @@ function ManageListingContainer() {
             .then(res => res.json())
             .then(data => {
                 if (data) {
-                    setInactiveMemberListings(data.inactiveListing);
+                    setViewableListingData(data.inactiveListing);
                 } else if (data.err) {
                     toast.error(data.err);
                 }
@@ -85,7 +98,7 @@ function ManageListingContainer() {
             .then(res => res.json())
             .then(data => {
                 if (data) {
-                    setLiveBusinessListings(data.liveListings);
+                    setViewableListingData(data.liveListings);
                 } else if (data.err) {
                     toast.error(data.err);
                 }
@@ -99,7 +112,7 @@ function ManageListingContainer() {
             .then(res => res.json())
             .then(data => {
                 if (data) {
-                    setInactiveBusinessListings(data.inactiveListing);
+                    setViewableListingData(data.inactiveListings)
                 } else if (data.err) {
                     toast.error(data.err);
                 }
@@ -113,7 +126,22 @@ function ManageListingContainer() {
             .then(res => res.json())
             .then(data => {
                 if (data) {
-                    setRejectedBusinessListings(data.inactiveListing);
+                    setViewableListingData(data.rejectedListings);
+                } else if (data.err) {
+                    toast.error(data.err);
+                }
+            }).catch(err => {
+            toast.error(err.message);
+        });
+    }
+
+
+    function getPendingBusinessListings() {
+        BusinessServices.getPendingBusinessListings()
+            .then(res => res.json())
+            .then(data => {
+                if (data) {
+                    setViewableListingData(data.pendingListings);
                 } else if (data.err) {
                     toast.error(data.err);
                 }
@@ -145,23 +173,25 @@ function ManageListingContainer() {
             <ManageListingTabs
                 setActiveTab={setSelectedTab}
             />
-
-            {/* Display cards once user has selected a tab i.e selectedTab has a value */}
-            {(!viewableListingData || !viewableListingData.length) && selectedTab ?
-                <Confirmation displayButton={false} errorColor={true} message={"No " + selectedTab + " Listings"}
-                              minHeight={false}/>
-                : <ManageListingResults
-                    onSubmit={deleteListing}
-                    activeTab={selectedTab}
-                    viewableListingData={viewableListingData}
-                    setViewableListingData={setViewableListingTitle}
-                />
-
-            }
+            {/* Display loading if screen is loading, display cards if there is card data & tab has been selected */}
+            {(loading ? <Loading isLoading={loading}/> :
+                    (viewableListingData && selectedTab) && <ManageListingResults
+                                                                onSubmit={deleteListing}
+                                                                activeTab={selectedTab}
+                                                                viewableListingData={(!loading && viewableListingData)}
+                                                                setViewableListingTitle={setViewableListingTitle}
+                                                            />
+            )}
         </div>
     );
 }
 
-ManageListingContainer.propTypes = {}
+const mapStateToProps = state => ({
+    accountType: state.userPrivileges.accountType
+});
 
-export default ManageListingContainer;
+ManageListingContainer.propTypes = {
+    accountType: PropTypes.string.isRequired,
+}
+
+export default compose(withRouter, connect(mapStateToProps, null))(ManageListingContainer);
