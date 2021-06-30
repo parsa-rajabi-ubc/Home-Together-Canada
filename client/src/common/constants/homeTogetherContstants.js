ddfac1dd04d70e81fa6349b6b2d7a938ee376ded
Rachelle Gelden <43148498+rachellegelden@users.noreply.github.com>
2021-06-29 20:59:36 -0600

    HTC-875: temp recover password solution (#880)
    * HTC-875: alert to inform users to contact HTC to reset passwords
    
    * HTC-875: route for admin to reset password
    
    * HTC-875: add a home together email constant

---
+++ client/src/common/constants/homeTogetherContstants.js
@@ -0,0 +1,9 @@
+/**
+ * @Author:     Rachelle Gelden
+ * @Created:    2020.12.20
+ *
+ * @Description: constants related to the Home Together Canada organization
+ *
+ */
+
+export const HOME_TOGETHER_EMAIL = 'info@hometogether.ca';
