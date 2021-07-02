5c0f64fa22d15a604e076672d1c9cd03f4a35f8b
Rachelle Gelden <43148498+rachellegelden@users.noreply.github.com>
2021-07-01 19:02:01 -0600

    HTC-903: update email in contact form (#909)

--- client/src/contact/Contact.js
+++ client/src/contact/Contact.js
@@ -7,6 +7,7 @@
  */
 
 import React from 'react';
+import {HOME_TOGETHER_EMAIL} from "../common/constants/homeTogetherContstants";
 
 function Contact(){
     return (
@@ -15,7 +16,7 @@ function Contact(){
             <h1 className={"page-title"}>Contact Us</h1>
 
             <h2 className={"point-title my-4"}>This page is currently under development.</h2>
-            <p className={"text"}>In the meantime, please direct all your questions, comments and concerns to Home Together Canada at: <a className={"link"} href="mailto:hometogether.ca@gmail.com">hometogether.ca@gmail.com</a></p>
+            <p className={"text"}>In the meantime, please direct all your questions, comments and concerns to Home Together Canada at: <a className={"link"} href={`mailto:${HOME_TOGETHER_EMAIL}`}>{HOME_TOGETHER_EMAIL}</a></p>
 
         </div>
     );
