357a05aa1581d6002a3ed542ce0f31a667fb8db6
parsa-rajabi <ParsaRajabiPR@gmail.com>
2023-01-10 08:39:11 -0800

    HTC-985: removed heroku db creds

--- server/db.config.js
+++ server/db.config.js
@@ -10,20 +10,7 @@ const personalConfig = require('./personalConfig/personalDbConfig');
 module.exports = {
     development: personalConfig.development,
     test: personalConfig.test,
-    // staging: {
-    //     DB: "heroku_a2e066b08e4b1b7",
-    //     USER: "b78602ef23aff8",
-    //     PASSWORD: "32764ac4",
-    //     HOST: "us-cdbr-east-02.cleardb.com",
-    //     POOL: {
-    //         max: 5,
-    //         min: 0,
-    //         acquire: 30000,
-    //         idle: 10000
-    //     },
-    //     DIALECT: "mysql",
-    //     PORT: 3306
-    // },
+    // replace heroku db connection with digitalocean db in Jan 2023, switched from heroku to railway.app for hosting staging env
     staging: {
         HOST: "hometogethercanadadatabase-do-user-9184613-0.b.db.ondigitalocean.com",
         PORT: 25060,
